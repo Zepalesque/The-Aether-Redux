ea8b7ecc4f1b766441ef3d0a461f065deb2dfca0
Zepalesque <60141811+Zepalesque@users.noreply.github.com>
2023-12-31 22:30:54 -0500

    fix: Missing wings model parts

--- src/main/java/net/zepalesque/redux/client/render/entity/model/entity/moa/MoaAdditionsModel.java
+++ src/main/java/net/zepalesque/redux/client/render/entity/model/entity/moa/MoaAdditionsModel.java
@@ -144,6 +144,34 @@ public class MoaAdditionsModel  extends EntityModel<Moa> {
         PartDefinition tail3_r1 = left_feather.addOrReplaceChild("tail3_r1", CubeListBuilder.create().texOffs(53, 43).addBox(-0.5F, 0.0F, 0.0F, 3.0F, 0.0F, 5.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(3.0F, 0.0F, 0.0F, 0.2618F, 0.5236F, 0.0F));
 
 
+        PartDefinition wing_1 = body_additions.addOrReplaceChild("wing_1", CubeListBuilder.create(), PartPose.offsetAndRotation(-4.1F, -2.0F, -2.0F, 1.5708F, 0.0F, 0.0F));
+
+        PartDefinition z_rot_wing_1 = wing_1.addOrReplaceChild("z_rot_wing_1", CubeListBuilder.create().texOffs(27, 20).addBox(-1.5F, -0.5F, -2.0F, 1.0F, 8.0F, 4.0F, new CubeDeformation(0.0F)), PartPose.offset(1.5F, -0.5F, 1.0F));
+
+        PartDefinition feathers_1_wing1 = z_rot_wing_1.addOrReplaceChild("feathers_1_wing1", CubeListBuilder.create().texOffs(11, 21).addBox(-0.75F, -1.0F, 0.0F, 0.0F, 2.0F, 6.0F, new CubeDeformation(0.0F)), PartPose.offset(-0.5F, 1.5F, -1.0F));
+
+        PartDefinition feathers_2_wing1 = z_rot_wing_1.addOrReplaceChild("feathers_2_wing1", CubeListBuilder.create(), PartPose.offset(-0.5F, 3.0F, -1.0F));
+
+        PartDefinition wing_feathers_r1 = feathers_2_wing1.addOrReplaceChild("wing_feathers_r1", CubeListBuilder.create().texOffs(11, 21).addBox(-0.75F, -1.0F, 0.0F, 0.0F, 2.0F, 6.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, 0.0F, 0.0F, -0.3054F, 0.0F, 0.0F));
+
+        PartDefinition feathers_3_wing1 = z_rot_wing_1.addOrReplaceChild("feathers_3_wing1", CubeListBuilder.create(), PartPose.offset(-0.5F, 4.5F, -1.0F));
+
+        PartDefinition wing_feathers_r2 = feathers_3_wing1.addOrReplaceChild("wing_feathers_r2", CubeListBuilder.create().texOffs(11, 20).addBox(-0.75F, -1.0F, 0.0F, 0.0F, 2.0F, 5.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, 0.0F, 0.0F, -0.6981F, 0.0F, 0.0F));
+
+        PartDefinition wing_2 = body_additions.addOrReplaceChild("wing_2", CubeListBuilder.create(), PartPose.offsetAndRotation(4.1F, -2.0F, -2.0F, 1.5708F, 0.0F, 0.0F));
+
+        PartDefinition z_rot_wing_2 = wing_2.addOrReplaceChild("z_rot_wing_2", CubeListBuilder.create().texOffs(27, 20).mirror().addBox(0.5F, -0.5F, -2.0F, 1.0F, 8.0F, 4.0F, new CubeDeformation(0.0F)).mirror(false), PartPose.offset(-1.5F, -0.5F, 1.0F));
+
+        PartDefinition feathers_1_wing2 = z_rot_wing_2.addOrReplaceChild("feathers_1_wing2", CubeListBuilder.create().texOffs(11, 21).mirror().addBox(0.75F, -1.0F, 0.0F, 0.0F, 2.0F, 6.0F, new CubeDeformation(0.0F)).mirror(false), PartPose.offset(0.5F, 1.5F, -1.0F));
+
+        PartDefinition feathers_2_wing2 = z_rot_wing_2.addOrReplaceChild("feathers_2_wing2", CubeListBuilder.create(), PartPose.offset(0.5F, 3.0F, -1.0F));
+
+        PartDefinition wing_feathers_r3 = feathers_2_wing2.addOrReplaceChild("wing_feathers_r3", CubeListBuilder.create().texOffs(11, 21).mirror().addBox(0.75F, -1.0F, 0.0F, 0.0F, 2.0F, 6.0F, new CubeDeformation(0.0F)).mirror(false), PartPose.offsetAndRotation(0.0F, 0.0F, 0.0F, -0.3054F, 0.0F, 0.0F));
+
+        PartDefinition feathers_3_wing2 = z_rot_wing_2.addOrReplaceChild("feathers_3_wing2", CubeListBuilder.create(), PartPose.offset(0.5F, 4.5F, -1.0F));
+
+        PartDefinition wing_feathers_r4 = feathers_3_wing2.addOrReplaceChild("wing_feathers_r4", CubeListBuilder.create().texOffs(11, 20).mirror().addBox(0.75F, -1.0F, 0.0F, 0.0F, 2.0F, 5.0F, new CubeDeformation(0.0F)).mirror(false), PartPose.offsetAndRotation(0.0F, 0.0F, 0.0F, -0.6981F, 0.0F, 0.0F));
+
         PartDefinition leg1 = base.addOrReplaceChild("leg1", CubeListBuilder.create(), PartPose.offset(4.0F, 7.0F, 2.0F));
 
         PartDefinition base_leg1 = leg1.addOrReplaceChild("base_leg1", CubeListBuilder.create(), PartPose.offset(0.0F, 0.0F, 0.0F));
