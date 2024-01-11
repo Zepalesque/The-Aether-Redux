791ef494d5626ae8f6a6992becb39bbc9344a9da
Zepalesque <60141811+Zepalesque@users.noreply.github.com>
2024-01-11 13:02:59 -0500

    fix: Valk queen no longer takes medals when grand medal is in inventory

--- src/main/java/net/zepalesque/redux/mixin/common/entity/ValkQueenMixin.java
+++ src/main/java/net/zepalesque/redux/mixin/common/entity/ValkQueenMixin.java
@@ -1,16 +1,17 @@
 package net.zepalesque.redux.mixin.common.entity;
 
 import com.aetherteam.aether.entity.monster.dungeon.boss.ValkyrieQueen;
-import com.aetherteam.aether.item.EquipmentUtil;
 import net.minecraft.world.entity.player.Inventory;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
 import net.zepalesque.redux.item.ReduxItems;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
 
-// TODO: Find a better way PLEASE
+// TODO: Find a better way PLEASE this is bad implementation
 @Mixin(ValkyrieQueen.class)
 public class ValkQueenMixin {
 
@@ -18,6 +19,13 @@ public class ValkQueenMixin {
     @Redirect(method = "handleNpcInteraction", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/player/Inventory;countItem(Lnet/minecraft/world/item/Item;)I"))
     public int initialize(Inventory instance, Item item) {
         return instance.countItem(ReduxItems.GRAND_VICTORY_MEDAL.get()) >= 1 ? 10 : instance.countItem(item);
+    }
+
+    @Redirect(method = "handleNpcInteraction", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/ItemStack;shrink(I)V"))
+    public void shrinkStack(ItemStack instance, int decrement, Player player) {
+        if (player.getInventory().countItem(ReduxItems.GRAND_VICTORY_MEDAL.get()) < 1) {
+            instance.shrink(decrement);
+        }
     }
 
 }
