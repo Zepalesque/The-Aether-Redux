b3358a1cd89b6ae9d48949a3b8452254721cc8c3
Zepalesque <60141811+Zepalesque@users.noreply.github.com>
2024-01-20 20:37:04 -0500

    feat: Post processing now uses it's own system which is seperate from
    - Also added a config for the post process shader

---
+++ src/main/java/net/zepalesque/redux/mixin/client/render/shader/PostPassMixin.java
@@ -0,0 +1,31 @@
+package net.zepalesque.redux.mixin.client.render.shader;
+
+import net.minecraft.client.GraphicsStatus;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.EffectInstance;
+import net.minecraft.client.renderer.GameRenderer;
+import net.minecraft.client.renderer.PostPass;
+import net.zepalesque.redux.capability.player.ReduxPlayer;
+import net.zepalesque.redux.client.ReduxPostProcessHandler;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+// TODO: Investigate whether or not this is a good way to do this, because I'm not convinced it is...
+@Mixin(PostPass.class)
+public class PostPassMixin {
+
+    @Shadow @Final private EffectInstance effect;
+
+    @Inject(at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/EffectInstance;apply()V", shift = At.Shift.BEFORE), method = "process")
+    private void processInvoke(float partialTicks, CallbackInfo ci) {
+        if (Minecraft.getInstance().player != null && Minecraft.getInstance().options.graphicsMode().get() == GraphicsStatus.FABULOUS) {
+            ReduxPlayer.get(Minecraft.getInstance().player).ifPresent(reduxPlayer -> this.effect.safeGetUniform("AetherRedux_AdrenalineStrength").set(reduxPlayer.getAdrenalineModule().getShaderStrength()));
+        } else {
+            this.effect.safeGetUniform("AetherRedux_AdrenalineStrength").set(0.0F);
+        }
+    }
+}
