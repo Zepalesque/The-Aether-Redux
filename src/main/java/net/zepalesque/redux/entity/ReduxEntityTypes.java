c831594ea208af6be1b7b3db72c9f15134152705
Zepalesque <60141811+Zepalesque@users.noreply.github.com>
2024-01-22 14:25:36 -0500

    fix: shimmercow loot should generate

--- src/main/java/net/zepalesque/redux/entity/ReduxEntityTypes.java
+++ src/main/java/net/zepalesque/redux/entity/ReduxEntityTypes.java
@@ -25,9 +25,9 @@ public class ReduxEntityTypes {
 
     public static final DeferredRegister<EntityType<?>> ENTITY_TYPES = DeferredRegister.create(ForgeRegistries.ENTITY_TYPES, Redux.MODID);
     
-    public static final RegistryObject<EntityType<VolatileFireCrystal>> VOLATILE_FIRE_CRYSTAL = ENTITY_TYPES.register("volatile_fire_crystal", () -> {
-        return EntityType.Builder.<VolatileFireCrystal>of(VolatileFireCrystal::new, MobCategory.MISC).sized(0.85F, 0.85F).clientTrackingRange(4).updateInterval(10).fireImmune().build("fire_crystal");
-    });
+    public static final RegistryObject<EntityType<VolatileFireCrystal>> VOLATILE_FIRE_CRYSTAL = ENTITY_TYPES.register("volatile_fire_crystal",
+            () -> EntityType.Builder.<VolatileFireCrystal>of(VolatileFireCrystal::new, MobCategory.MISC).sized(0.85F, 0.85F).clientTrackingRange(4).updateInterval(10).fireImmune().build("fire_crystal")
+    );
     public static final RegistryObject<EntityType<Swet>> VANILLA_SWET = ENTITY_TYPES.register("vanilla_swet",
             () -> EntityType.Builder.of(Swet::new, AetherMobCategory.AETHER_SURFACE_MONSTER).sized(0.9F, 0.95F).clientTrackingRange(10).build("vanilla_swet"));
 
@@ -38,7 +38,7 @@ public class ReduxEntityTypes {
             () -> EntityType.Builder.<Ember>of(Ember::new, MobCategory.MISC).sized(0.125F, 0.125F).clientTrackingRange(4).updateInterval(20).build("ember"));
 
     public static final RegistryObject<EntityType<Shimmercow>> SHIMMERCOW = ENTITY_TYPES.register("shimmercow",
-            () -> EntityType.Builder.<Shimmercow>of(Shimmercow::new, MobCategory.MISC).sized(1.125F, 1.625F).clientTrackingRange(10).build("shimmercow"));
+            () -> EntityType.Builder.<Shimmercow>of(Shimmercow::new, MobCategory.CREATURE).sized(1.125F, 1.625F).clientTrackingRange(10).build("shimmercow"));
 
     @SubscribeEvent
     public static void registerSpawnPlacements(SpawnPlacementRegisterEvent event) {
