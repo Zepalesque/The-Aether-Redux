e8e4c1d7778dd6b50326e905ae71221b49425187
Zepalesque <60141811+Zepalesque@users.noreply.github.com>
2024-01-24 16:14:04 -0500

    refactor: redo tweak

--- src/main/java/net/zepalesque/redux/data/resource/ReduxConfiguredFeatures.java
+++ src/main/java/net/zepalesque/redux/data/resource/ReduxConfiguredFeatures.java
@@ -392,8 +392,8 @@ public class    ReduxConfiguredFeatures {
                         prov(Redux.Handlers.Wood.CLOUDCAP.log),
                         prov(naturalDrops(Redux.Handlers.Wood.CLOUDCAP.logWall)),
                         UniformInt.of(11, 17),
+                        UniformInt.of(1, 4),
                         UniformInt.of(1, 3),
-                        UniformInt.of(1, 2),
                         UniformInt.of(6, 9))
         );
         register(context, LARGE_MUSHROOMS, Feature.RANDOM_SELECTOR,
