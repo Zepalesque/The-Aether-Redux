4beaaadd77fa73b1c60cbe4e87a436e41206fa10
Zepalesque <60141811+Zepalesque@users.noreply.github.com>
2024-01-24 17:14:29 -0500

    fix: Fix log wall shape map

--- src/main/java/net/zepalesque/redux/block/natural/ReduxNaturalWall.java
+++ src/main/java/net/zepalesque/redux/block/natural/ReduxNaturalWall.java
@@ -1,10 +1,15 @@
 package net.zepalesque.redux.block.natural;
 
 import com.aetherteam.aether.block.AetherBlockStateProperties;
+import com.google.common.collect.ImmutableMap;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.phys.shapes.VoxelShape;
 import net.zepalesque.redux.block.util.ReduxStates;
+import net.zepalesque.redux.mixin.common.block.ReduxWallBlockAccessor;
+
+import java.util.Map;
 
 public class ReduxNaturalWall extends ReduxDoubleDropsWall {
     public ReduxNaturalWall(Properties properties) {
@@ -16,5 +21,26 @@ public class ReduxNaturalWall extends ReduxDoubleDropsWall {
     protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
         super.createBlockStateDefinition(builder);
         builder.add(ReduxStates.NATURAL_GEN);
+        this.fixShapeMaps();
+    }
+
+    private void fixShapeMaps() {
+        ReduxWallBlockAccessor reduxWallBlockAccessor = (ReduxWallBlockAccessor) this;
+        Map<BlockState, VoxelShape> shapeByIndex = reduxWallBlockAccessor.getShapeByIndex();
+        shapeByIndex = fixShapeMap(shapeByIndex);
+        reduxWallBlockAccessor.setShapeByIndex(shapeByIndex);
+
+        Map<BlockState, VoxelShape> collisionShapeByIndex = reduxWallBlockAccessor.getCollisionShapeByIndex();
+        collisionShapeByIndex = fixShapeMap(collisionShapeByIndex);
+        reduxWallBlockAccessor.setCollisionShapeByIndex(collisionShapeByIndex);
+    }
+
+    private static Map<BlockState, VoxelShape> fixShapeMap(Map<BlockState, VoxelShape> map) {
+        ImmutableMap.Builder<BlockState, VoxelShape> builder = ImmutableMap.builder();
+        builder.putAll(map);
+        for (BlockState state : map.keySet()) {
+            builder.put(state.cycle(ReduxStates.NATURAL_GEN), map.get(state));
+        }
+        return builder.build();
     }
 }
