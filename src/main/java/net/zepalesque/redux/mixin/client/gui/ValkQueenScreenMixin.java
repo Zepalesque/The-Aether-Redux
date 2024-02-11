bcf898859b03c3fa8455870e3c50a0d83c6cfc90
Zepalesque <60141811+Zepalesque@users.noreply.github.com>
2024-02-11 12:38:53 -0500

    feat: fix god-awful mixin
    - thanks to jonathing for the improved code lol

--- src/main/java/net/zepalesque/redux/mixin/client/gui/ValkQueenScreenMixin.java
+++ src/main/java/net/zepalesque/redux/mixin/client/gui/ValkQueenScreenMixin.java
@@ -2,52 +2,21 @@ package net.zepalesque.redux.mixin.client.gui;
 
 import com.aetherteam.aether.client.gui.component.dialogue.DialogueChoiceComponent;
 import com.aetherteam.aether.client.gui.screen.ValkyrieQueenDialogueScreen;
-import com.aetherteam.aether.item.AetherItems;
-import com.aetherteam.aether.item.EquipmentUtil;
-import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.network.chat.Component;
-import net.minecraft.network.chat.MutableComponent;
-import net.minecraft.world.item.Item;
 import net.zepalesque.redux.item.ReduxItems;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
-// TODO: Investigate a better solution
 @Mixin(ValkyrieQueenDialogueScreen.class)
 public abstract class ValkQueenScreenMixin extends ScreenMixin {
 
-    @Shadow public abstract MutableComponent buildDialogueChoice(String key);
-
     @Shadow protected abstract void finishChat(byte interactionID);
 
-    @Shadow protected abstract void setDialogueAnswer(Component component);
-
-    @Shadow public abstract void setupDialogueChoices(DialogueChoiceComponent... options);
-
-    @Redirect(method = "init", at = @At(value = "INVOKE", target = "Lcom/aetherteam/aether/client/gui/screen/ValkyrieQueenDialogueScreen;setupDialogueChoices([Lcom/aetherteam/aether/client/gui/component/dialogue/DialogueChoiceComponent;)V"))
-    public void initialize(ValkyrieQueenDialogueScreen instance, DialogueChoiceComponent... options) {
-        this.setupDialogueChoices(new DialogueChoiceComponent(this.buildDialogueChoice("question"), (button) -> {
-            this.finishChat((byte)0);
-        }), new DialogueChoiceComponent(this.buildDialogueChoice("challenge"), (button) -> {
-            this.setDialogueAnswer(Component.translatable("gui.aether.queen.dialog.challenge"));
-            int medals = this.getMinecraft().player.getInventory().countItem((Item) AetherItems.VICTORY_MEDAL.get());
-            /* injected line */ boolean hasGrandMedal = this.getMinecraft().player.getInventory().countItem(ReduxItems.GRAND_VICTORY_MEDAL.get()) >= 1;
-            DialogueChoiceComponent startFightChoice =
-            /* injected line */ hasGrandMedal ? new DialogueChoiceComponent(Component.translatable("gui.aether_redux.player.dialog.has_grand_medal"), (button1) -> {this.finishChat((byte)1);}) :
-            medals >= 10 ? new DialogueChoiceComponent(this.buildDialogueChoice("have_medals"), (button1) -> {
-                this.finishChat((byte)1);
-            }) : new DialogueChoiceComponent(this.buildDialogueChoice("no_medals").append(" (" + medals + "/10)"), (button1) -> {
-                this.finishChat((byte)1);
-            });
-            this.setupDialogueChoices(startFightChoice, new DialogueChoiceComponent(this.buildDialogueChoice("deny_fight"), (button1) -> {
-                this.finishChat((byte)2);
-            }));
-        }), new DialogueChoiceComponent(this.buildDialogueChoice("leave"), (pButton) -> {
-            this.finishChat((byte)3);
-        }));
+    @ModifyVariable(method = "lambda$init$4", at = @At(value = "LOAD"), index = 3)
+    private DialogueChoiceComponent startFightChoice$modify(DialogueChoiceComponent original) {
+        boolean hasGrandMedal = this.getMinecraft().player.getInventory().countItem(ReduxItems.GRAND_VICTORY_MEDAL.get()) >= 1;
+        return hasGrandMedal ? new DialogueChoiceComponent(Component.translatable("gui.aether_redux.player.dialog.has_grand_medal"), (button1) -> this.finishChat((byte)1)) : original;
     }
 }
