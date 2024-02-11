c2365d878c041de07c6f87bfa12f13c555a103e0
Zepalesque <60141811+Zepalesque@users.noreply.github.com>
2024-02-11 12:42:56 -0500

    fix: More mixin stuff

--- src/main/java/net/zepalesque/redux/mixin/client/render/MimicRendererMixin.java
+++ src/main/java/net/zepalesque/redux/mixin/client/render/MimicRendererMixin.java
@@ -3,6 +3,7 @@ package net.zepalesque.redux.mixin.client.render;
 import com.aetherteam.aether.client.renderer.entity.MimicRenderer;
 import com.aetherteam.aether.client.renderer.entity.model.MimicModel;
 import com.aetherteam.aether.entity.monster.dungeon.Mimic;
+import com.aetherteam.aether_genesis.client.renderer.entity.SkyrootMimicRenderer;
 import com.aetherteam.aether_genesis.entity.monster.BattleSentry;
 import com.mojang.blaze3d.vertex.PoseStack;
 import net.minecraft.client.renderer.MultiBufferSource;
@@ -10,7 +11,10 @@ import net.zepalesque.redux.config.ReduxConfig;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(MimicRenderer.class)
+@Mixin({
+        MimicRenderer.class,
+        SkyrootMimicRenderer.class
+})
 public class MimicRendererMixin extends MobRendererMixin<Mimic, MimicModel> {
     @Override
     public void renderMob(Mimic mimic, float entityYaw, float partialTicks, PoseStack poseStack, MultiBufferSource buffer, int packedLight, CallbackInfo ci) {
