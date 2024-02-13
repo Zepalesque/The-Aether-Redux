b882fcbcc89003aa33d97b2e2c1da51121f03f12
Zepalesque <60141811+Zepalesque@users.noreply.github.com>
2024-02-12 22:03:09 -0500

    fix: Feather of Warding now affects Blighted Aerclouds properly

--- src/main/java/net/zepalesque/redux/block/natural/blight/BlightedAercloudBlock.java
+++ src/main/java/net/zepalesque/redux/block/natural/blight/BlightedAercloudBlock.java
@@ -2,6 +2,7 @@ package net.zepalesque.redux.block.natural.blight;
 
 import com.aetherteam.aether.block.natural.AercloudBlock;
 import com.aetherteam.aether.effect.AetherEffects;
+import com.aetherteam.aether.item.EquipmentUtil;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectInstance;
@@ -10,6 +11,8 @@ import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
 import net.zepalesque.redux.data.resource.ReduxDamageTypes;
+import net.zepalesque.redux.event.hook.EquipmentHooks;
+import net.zepalesque.redux.item.ReduxItems;
 import net.zepalesque.redux.misc.ReduxTags;
 
 public class BlightedAercloudBlock extends AercloudBlock {
@@ -21,7 +24,7 @@ public class BlightedAercloudBlock extends AercloudBlock {
     public void entityInside(BlockState state, Level world, BlockPos pos, Entity entity) {
         super.entityInside(state, world, pos, entity);
         MobEffect effect = AetherEffects.INEBRIATION.get();
-        if (entity instanceof LivingEntity livingEntity) {
+        if (entity instanceof LivingEntity livingEntity && EquipmentUtil.hasCurio(livingEntity, ReduxItems.FEATHER_OF_WARDING.get())) {
             if (livingEntity.tickCount % (livingEntity.hasEffect(effect) ? 10 : 20) == 0) {
                 if (livingEntity.getType().is(ReduxTags.EntityTypes.BLIGHTED_MOBS)) {
                     livingEntity.heal(1.0F);
