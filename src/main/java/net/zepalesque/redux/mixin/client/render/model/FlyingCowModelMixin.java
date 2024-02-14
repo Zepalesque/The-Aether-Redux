cca4ad0509446ac4ea1f64f2eb403a983029285b
Zepalesque <60141811+Zepalesque@users.noreply.github.com>
2024-02-14 18:06:45 -0500

    fix: well that didn't work

--- src/main/java/net/zepalesque/redux/mixin/client/render/model/FlyingCowModelMixin.java
+++ src/main/java/net/zepalesque/redux/mixin/client/render/model/FlyingCowModelMixin.java
@@ -1,9 +1,11 @@
 package net.zepalesque.redux.mixin.client.render.model;
 
 import com.aetherteam.aether.client.renderer.entity.model.SheepuffModel;
+import com.aetherteam.aether.entity.AetherEntityTypes;
 import com.aetherteam.aether.entity.passive.FlyingCow;
 import com.aetherteam.aether.entity.passive.Sheepuff;
 import net.minecraft.client.model.CowModel;
+import net.minecraft.world.entity.Entity;
 import net.zepalesque.redux.config.ReduxConfig;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -11,9 +13,12 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(CowModel.class)
-public class FlyingCowModelMixin extends QuadrupedModelMixin<FlyingCow> {
+public class FlyingCowModelMixin extends QuadrupedModelMixin<Entity> {
+
     @Override
-    public void setupAnim(FlyingCow entity, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch, CallbackInfo ci) {
-        this.head.skipDraw = ReduxConfig.CLIENT.flying_cow_model_upgrade.get();
+    public void setupAnim(Entity entity, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch, CallbackInfo ci) {
+        if (entity.getType() == AetherEntityTypes.FLYING_COW.get()) {
+            this.head.skipDraw = ReduxConfig.CLIENT.flying_cow_model_upgrade.get();
+        }
     }
 }
