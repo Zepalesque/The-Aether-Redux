26e2d3cbeffcf5a3519247dff15a31e772fa34d6
Zepalesque <60141811+Zepalesque@users.noreply.github.com>
2024-02-14 22:28:51 -0500

    refactor: Fix grass mixin not working
    - The Aether's grass block pretty much copies the vanilla method, but the fact that it overrides it made extending a seperate mixin not work correctly

--- src/main/java/net/zepalesque/redux/mixin/common/block/AetherGrassBlockMixin.java
+++ src/main/java/net/zepalesque/redux/mixin/common/block/AetherGrassBlockMixin.java
@@ -2,6 +2,7 @@ package net.zepalesque.redux.mixin.common.block;
 
 import com.aetherteam.aether.block.natural.AetherGrassBlock;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Holder;
 import net.minecraft.core.registries.Registries;
@@ -12,24 +13,25 @@ import net.minecraft.world.level.chunk.ChunkGenerator;
 import net.minecraft.world.level.levelgen.placement.PlacedFeature;
 import net.zepalesque.redux.block.ReduxBlocks;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
 
 import java.util.Optional;
 
 @Mixin(AetherGrassBlock.class)
 public abstract class AetherGrassBlockMixin extends GrassBlockMixin {
 
-    @Override
-    protected boolean placeGrass(PlacedFeature instance, WorldGenLevel pLevel, ChunkGenerator pGenerator, RandomSource pRandom, BlockPos pPos, Operation<Boolean> original) {
+    @WrapOperation(method = "performBonemeal", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/levelgen/placement/PlacedFeature;place(Lnet/minecraft/world/level/WorldGenLevel;Lnet/minecraft/world/level/chunk/ChunkGenerator;Lnet/minecraft/util/RandomSource;Lnet/minecraft/core/BlockPos;)Z"))
+    protected boolean placeGrass(PlacedFeature instance, WorldGenLevel level, ChunkGenerator generator, RandomSource random, BlockPos pos, Operation<Boolean> original) {
 
-        Optional<Holder.Reference<PlacedFeature>> optional = pLevel.registryAccess().registryOrThrow(Registries.PLACED_FEATURE).getHolder(VegetationPlacements.GRASS_BONEMEAL);
+        Optional<Holder.Reference<PlacedFeature>> optional = level.registryAccess().registryOrThrow(Registries.PLACED_FEATURE).getHolder(VegetationPlacements.GRASS_BONEMEAL);
         if (instance.equals(optional.get().value())) {
-            if (pLevel.ensureCanWrite(pPos)) {
-                pLevel.setBlock(pPos, ReduxBlocks.SHORT_AETHER_GRASS.get().defaultBlockState(), 3);
+            if (level.ensureCanWrite(pos)) {
+                level.setBlock(pos, ReduxBlocks.SHORT_AETHER_GRASS.get().defaultBlockState(), 3);
                 return true;
             } else {
                 return false;
             }
         }
-        return super.placeGrass(instance, pLevel, pGenerator, pRandom, pPos, original);
+        return original.call(instance, level, generator, random, pos);
     }
 }
