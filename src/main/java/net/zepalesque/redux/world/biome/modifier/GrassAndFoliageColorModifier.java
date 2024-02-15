f0fe5854d37dc6af1772744efa8b424a148c302e
Zepalesque <60141811+Zepalesque@users.noreply.github.com>
2024-02-15 13:44:11 -0500

    refactor: hopefully working now

--- src/main/java/net/zepalesque/redux/world/biome/modifier/GrassAndFoliageColorModifier.java
+++ src/main/java/net/zepalesque/redux/world/biome/modifier/GrassAndFoliageColorModifier.java
@@ -7,14 +7,16 @@ import net.minecraft.world.level.biome.Biome;
 import net.minecraftforge.common.world.BiomeModifier;
 import net.minecraftforge.common.world.ModifiableBiomeInfo;
 
+import java.util.Optional;
+
 public record GrassAndFoliageColorModifier(HolderSet<Biome> biomes,  int grass, int foliage) implements BiomeModifier {
     @Override
     public void modify(Holder<Biome> biome, Phase phase, ModifiableBiomeInfo.BiomeInfo.Builder builder) {
         if (phase == Phase.AFTER_EVERYTHING && biomes.contains(biome)) {
             builder.getSpecialEffects().grassColorOverride(grass);
             builder.getSpecialEffects().foliageColorOverride(foliage);
-            biome.get().getSpecialEffects().grassColorOverride = grass;
-            biome.get().getSpecialEffects().foliageColorOverride = foliage;
+            biome.get().getSpecialEffects().grassColorOverride = Optional.of(grass);
+            biome.get().getSpecialEffects().foliageColorOverride = Optional.of(foliage);
         }
 
     }
