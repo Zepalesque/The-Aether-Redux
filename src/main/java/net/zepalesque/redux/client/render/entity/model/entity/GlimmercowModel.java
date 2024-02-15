3cd40afe92176c676dfb4bc7952351b92d25c530
Zepalesque <60141811+Zepalesque@users.noreply.github.com>
2024-02-14 19:24:36 -0500

    fix: Hopefully fixed glimmercow model

--- src/main/java/net/zepalesque/redux/client/render/entity/model/entity/GlimmercowModel.java
+++ src/main/java/net/zepalesque/redux/client/render/entity/model/entity/GlimmercowModel.java
@@ -19,7 +19,9 @@ public class GlimmercowModel<T extends Glimmercow> extends QuadrupedModel   <T> 
       MeshDefinition meshdefinition = new MeshDefinition();
       PartDefinition partdefinition = meshdefinition.getRoot();
 
-      PartDefinition body = partdefinition.addOrReplaceChild("body", CubeListBuilder.create().texOffs(0, 0).addBox(-8.0F, -10.0F, -10.0F, 16.0F, 18.0F, 14.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 5.0F, 2.0F));
+      PartDefinition body = partdefinition.addOrReplaceChild("body", CubeListBuilder.create(), PartPose.offset(0.0F, 5.0F, 2.0F));
+      body.addOrReplaceChild("body_r1", CubeListBuilder.create().texOffs(31, 35).addBox(-4.0F, 2.0F, -11.0F, 8.0F, 6.0F, 1.0F, new CubeDeformation(0.0F))
+              .texOffs(0, 0).addBox(-8.0F, -10.0F, -10.0F, 16.0F, 18.0F, 14.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, 0.0F, 0.0F, 1.5708F, 0.0F, 0.0F));
 
       body.addOrReplaceChild("udder", CubeListBuilder.create().texOffs(32, 35).addBox(-3.0F, -45.0F, -9.0F, 6.0F, 6.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 47.0F, -2.0F));
 
