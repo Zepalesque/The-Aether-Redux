293e0b07d54c3bfc6faf2d3452be9d6fd6e2833b
Zepalesque <60141811+Zepalesque@users.noreply.github.com>
2024-02-16 20:43:47 -0500

    refactor: tweaks + ice shard textures

--- src/main/java/net/zepalesque/redux/mixin/common/entity/ArrowMixin.java
+++ src/main/java/net/zepalesque/redux/mixin/common/entity/ArrowMixin.java
@@ -1,14 +1,25 @@
 package net.zepalesque.redux.mixin.common.entity;
 
+import com.aetherteam.aether.entity.monster.dungeon.Mimic;
+import com.llamalad7.mixinextras.injector.v2.WrapWithCondition;
+import net.minecraft.core.particles.ParticleOptions;
 import net.minecraft.core.particles.ParticleTypes;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.projectile.AbstractArrow;
+import net.minecraft.world.level.Level;
+import net.minecraftforge.common.util.LazyOptional;
+import net.zepalesque.redux.capability.animation.mimic.MimicAnimation;
 import net.zepalesque.redux.capability.arrow.SubzeroArrow;
+import net.zepalesque.redux.config.ReduxConfig;
 import net.zepalesque.redux.network.ReduxPacketHandler;
 import net.zepalesque.redux.network.packet.SubzeroArrowPacket;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.Slice;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(AbstractArrow.class)
@@ -25,11 +36,34 @@ public class ArrowMixin
                 subzeroArrow.tick();
                 if (!arrow.level().isClientSide) {
                     ReduxPacketHandler.sendToAll(new SubzeroArrowPacket(arrow.getId(), true));
-                } else if (!this.inGround) {
-                    arrow.level().addParticle(ParticleTypes.SNOWFLAKE, arrow.getX(), arrow.getY(), arrow.getZ(), 0.0D, 0.0D, 0.0D);
                 }
             }
         });
     }
+
+
+    @WrapWithCondition(method = "tick", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/Level;addParticle(Lnet/minecraft/core/particles/ParticleOptions;DDDDDD)V"),
+    slice = @Slice(
+            from = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/projectile/AbstractArrow;isCritArrow()Z"),
+            to = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/projectile/AbstractArrow;isInWater()Z"))
+    )
+    private boolean replaceParticle(Level instance, ParticleOptions particleData, double x, double y, double z, double xSpeed, double ySpeed, double zSpeed) {
+        return this.particleCheck(instance, particleData, x, y, z, xSpeed, ySpeed, zSpeed);
+    }
+
+    @Unique
+    private boolean particleCheck(Level instance, ParticleOptions particleData, double x, double y, double z, double xSpeed, double ySpeed, double zSpeed) {
+        AbstractArrow arrow = (AbstractArrow) (Object) this;
+        LazyOptional<SubzeroArrow> subz = SubzeroArrow.get(arrow);
+
+        if (particleData.getType() == ParticleTypes.CRIT && subz.isPresent()
+                && subz.orElseThrow(() -> new IllegalStateException("Subzero arrow capability was not present right after checking to ensure it's present??? This should not be possible!"))
+                .isSubzeroArrow()) {
+            instance.addParticle(ParticleTypes.SNOWFLAKE /* TODO (maybe?) */, x, y, z, xSpeed, ySpeed,  zSpeed);
+            return false;
+        }
+        return true;
+    }
+
 
 }
