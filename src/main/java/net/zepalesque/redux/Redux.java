8230cc7a43ac6ab08626db8750c53929bedd341c
Zepalesque <60141811+Zepalesque@users.noreply.github.com>
2024-02-22 23:10:16 -0500

    gradle: Update version

--- src/main/java/net/zepalesque/redux/Redux.java
+++ src/main/java/net/zepalesque/redux/Redux.java
@@ -120,7 +120,7 @@ public class Redux
     @Nullable
     public static ReduxPackConfig packConfig;
 
-    public static final String VERSION_ID = "2.0-pre5";
+    public static final String VERSION_ID = "2.0-pre6";
 
     public Redux()
     {
