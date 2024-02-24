d6fbb9c2a7748ef53947badfeb171469a979c417
Zepalesque <60141811+Zepalesque@users.noreply.github.com>
2024-02-24 17:56:21 -0500

    fix: Fixes

--- src/main/java/net/zepalesque/redux/api/condition/Conditions.java
+++ src/main/java/net/zepalesque/redux/api/condition/Conditions.java
@@ -7,13 +7,13 @@ public class Conditions {
     public static final AbstractCondition<?> GENESIS = new ModLoaded("aether_genesis");
     public static final AbstractCondition<?> LOST = new ModLoaded("lost_aether_content");
     public static final AbstractCondition<?> ANCIENT = new ModLoaded("ancient_aether");
-    public static final AbstractCondition<?> GUMMY_NERF = ReduxConfigCondition.of(ReduxConfig.COMMON.nerf_gummy_swets);
-    public static final AbstractCondition<?> MOSSY_ORE = ReduxConfigCondition.of(ReduxConfig.COMMON.mossy_holystone_ores);
-    public static final AbstractCondition<?> RAW_GRAVITITE = ReduxConfigCondition.of(ReduxConfig.COMMON.raw_gravitite);
-    public static final AbstractCondition<?> ENCHGRASS = ReduxConfigCondition.of(ReduxConfig.COMMON.enchanted_gilded_grass);
-    public static final AbstractCondition<?> WATER = ReduxConfigCondition.of(ReduxConfig.COMMON.better_water_color);
-    public static final AbstractCondition<?> VINES = ReduxConfigCondition.of(ReduxConfig.COMMON.enchanted_vines);
-    public static final AbstractCondition<?> ALT_GILDED = ReduxConfigCondition.of(ReduxConfig.COMMON.alternate_gilded_trees);
-    public static final AbstractCondition<?> OVERRIDE_AA = ReduxConfigCondition.of(ReduxConfig.COMMON.override_aa_cloud_compat);
+    public static final AbstractCondition<?> GUMMY_NERF = new ReduxConfigCondition(ReduxConfig.COMMON.nerf_gummy_swets);
+    public static final AbstractCondition<?> MOSSY_ORE = new ReduxConfigCondition(ReduxConfig.COMMON.mossy_holystone_ores);
+    public static final AbstractCondition<?> RAW_GRAVITITE = new ReduxConfigCondition(ReduxConfig.COMMON.raw_gravitite);
+    public static final AbstractCondition<?> ENCHGRASS = new ReduxConfigCondition(ReduxConfig.COMMON.enchanted_gilded_grass);
+    public static final AbstractCondition<?> WATER = new ReduxConfigCondition(ReduxConfig.COMMON.better_water_color);
+    public static final AbstractCondition<?> VINES = new ReduxConfigCondition(ReduxConfig.COMMON.enchanted_vines);
+    public static final AbstractCondition<?> ALT_GILDED = new ReduxConfigCondition(ReduxConfig.COMMON.alternate_gilded_trees);
+    public static final AbstractCondition<?> OVERRIDE_AA = new ReduxConfigCondition(ReduxConfig.COMMON.override_aa_cloud_compat);
     public static final AbstractCondition<?> CLOUD_LAYER = new Or<>(new Not<>(Conditions.ANCIENT), Conditions.OVERRIDE_AA);
 }
