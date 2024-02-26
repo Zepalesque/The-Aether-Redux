7a026f9f070dd5a2a5c38cbd6dd3587f4095c2c5
Zepalesque <60141811+Zepalesque@users.noreply.github.com>
2024-02-25 21:52:58 -0500

    refactor: Alter text color

--- src/main/java/net/zepalesque/redux/Redux.java
+++ src/main/java/net/zepalesque/redux/Redux.java
@@ -118,7 +118,7 @@ public class Redux
     public static final String MODID = "aether_redux";
     public static final String DISPLAY = "The Aether: Redux";
     public static final Logger LOGGER = LogUtils.getLogger();
-    public static final int REDUX_PURPLE = 0x8C8CFF;
+    public static final int REDUX_PURPLE = 0x9384F4;
     @Nullable
     public static ReduxPackConfig packConfig;
 
