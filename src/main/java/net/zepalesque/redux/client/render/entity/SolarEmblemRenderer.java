3deba7573ce85d9229aeca4a0b4d61f2c21925a9
Zepalesque <60141811+Zepalesque@users.noreply.github.com>
2024-02-27 14:29:27 -0500

    feat: Solar emblem now renders two when wearing two of them

--- src/main/java/net/zepalesque/redux/client/render/entity/SolarEmblemRenderer.java
+++ src/main/java/net/zepalesque/redux/client/render/entity/SolarEmblemRenderer.java
@@ -1,5 +1,6 @@
 package net.zepalesque.redux.client.render.entity;
 
+import com.aetherteam.aether.item.EquipmentUtil;
 import com.aetherteam.aether.item.accessories.pendant.PendantItem;
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
@@ -16,12 +17,14 @@ import net.minecraft.world.item.ItemStack;
 import net.zepalesque.redux.Redux;
 import net.zepalesque.redux.client.render.ReduxModelLayers;
 import net.zepalesque.redux.client.render.entity.model.PinModel;
+import net.zepalesque.redux.item.ReduxItems;
 import top.theillusivec4.curios.api.SlotContext;
 import top.theillusivec4.curios.api.client.ICurioRenderer;
 
 public class SolarEmblemRenderer implements ICurioRenderer {
     private final PinModel pin;
     ResourceLocation LOC = Redux.locate("textures/models/accessory/misc/solar_emblem_accessory.png");
+    ResourceLocation DUAL_LOC = Redux.locate("textures/models/accessory/misc/solar_emblem_accessory_dual.png");
 
     public SolarEmblemRenderer() {
         this.pin = new PinModel(Minecraft.getInstance().getEntityModels().bakeLayer(ReduxModelLayers.PIN));
@@ -29,7 +32,7 @@ public class SolarEmblemRenderer implements ICurioRenderer {
 
     public <T extends LivingEntity, M extends EntityModel<T>> void render(ItemStack stack, SlotContext slotContext, PoseStack poseStack, RenderLayerParent<T, M> renderLayerParent, MultiBufferSource buffer, int packedLight, float limbSwing, float limbSwingAmount, float partialTicks, float ageInTicks, float netHeadYaw, float headPitch) {
         ICurioRenderer.followBodyRotations(slotContext.entity(), this.pin);
-        VertexConsumer consumer = ItemRenderer.getArmorFoilBuffer(buffer, RenderType.armorCutoutNoCull(LOC), false, stack.isEnchanted());
+        VertexConsumer consumer = ItemRenderer.getArmorFoilBuffer(buffer, RenderType.armorCutoutNoCull(EquipmentUtil.getCurios(slotContext.entity(), ReduxItems.SOLAR_EMBLEM.get()).size() >= 2 ? DUAL_LOC : LOC), false, stack.isEnchanted());
         this.pin.renderToBuffer(poseStack, consumer, packedLight, OverlayTexture.NO_OVERLAY, 1.0F, 1.0F, 1.0F, 1.0F);
     }
 }
