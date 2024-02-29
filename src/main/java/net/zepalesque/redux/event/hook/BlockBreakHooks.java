c2e4f9af8606b336033b9082fd58efce28cffdc6
Zepalesque <60141811+Zepalesque@users.noreply.github.com>
2024-02-29 13:20:57 -0500

    fix: stuff

--- src/main/java/net/zepalesque/redux/event/hook/BlockBreakHooks.java
+++ src/main/java/net/zepalesque/redux/event/hook/BlockBreakHooks.java
@@ -47,6 +47,7 @@ public class BlockBreakHooks {
             .put(AetherBlocks.AETHER_FARMLAND.get(), getSpeed(Blocks.FARMLAND))
             .put(AetherBlocks.AETHER_DIRT_PATH.get(), getSpeed(Blocks.DIRT_PATH))
             .put(AetherBlocks.AETHER_GRASS_BLOCK.get(), getSpeed(Blocks.GRASS_BLOCK))
+            .put(AetherBlocks.ENCHANTED_AETHER_GRASS_BLOCK.get(), getSpeed(Blocks.GRASS_BLOCK))
             .put(ReduxBlocks.AVELIUM.get(), getSpeed(Blocks.MYCELIUM))
             .put(ReduxBlocks.COARSE_AETHER_DIRT.get(), getSpeed(Blocks.COARSE_DIRT))
             .put(ReduxBlocks.GILDED_HOLYSTONE.get(), 1.0F)
