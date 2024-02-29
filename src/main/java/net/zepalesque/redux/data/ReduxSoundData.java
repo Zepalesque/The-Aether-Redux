2704d061a6edc9b5672f900432d80aee198af2dd
Zepalesque <60141811+Zepalesque@users.noreply.github.com>
2024-02-29 18:55:19 -0500

    fix: well that's not annoying at all

--- src/main/java/net/zepalesque/redux/data/ReduxSoundData.java
+++ src/main/java/net/zepalesque/redux/data/ReduxSoundData.java
@@ -110,7 +110,7 @@ public class ReduxSoundData extends SoundDefinitionsProvider {
                 definition().with(sound("aether_redux:item/infuse_item"))
                         .subtitle("subtitles.aether_redux.item.infuse_item"));
         this.add(ReduxSoundEvents.INFUSION_EXPIRE,
-                definition().with(sound("aether_redux:item/infuse_expire"))
+                definition().with(sound("aether_redux:item/infusion_expire"))
                         .subtitle("subtitles.aether_redux.item.infusion_expire"));
         this.add(ReduxSoundEvents.SPEAR_THROW,
                 definition().with(sound("aether_redux:item/spear_throw"))
