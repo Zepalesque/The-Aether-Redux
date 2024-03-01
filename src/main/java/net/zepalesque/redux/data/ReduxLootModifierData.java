3f56127f9515b122c63ba8e1fa8936a0e10a3390
Zepalesque <60141811+Zepalesque@users.noreply.github.com>
2024-02-29 22:28:16 -0500

    change: Grand medal has a 65% droprate now

--- src/main/java/net/zepalesque/redux/data/ReduxLootModifierData.java
+++ src/main/java/net/zepalesque/redux/data/ReduxLootModifierData.java
@@ -143,6 +143,7 @@ public class ReduxLootModifierData extends GlobalLootModifierProvider {
                 },
                 new LootItemCondition[] {
                         LootItemEntityPropertyCondition.hasProperties(LootContext.EntityTarget.THIS, new EntityPredicate.Builder().of(AetherEntityTypes.VALKYRIE_QUEEN.get())).build(),
+                        LootItemRandomChanceCondition.randomChance(0.65F).build()
                 }));
 
         this.add("solar_emblem", new DungeonLootModifier(
