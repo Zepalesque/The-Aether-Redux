10de33b00f4e350f215ef5c523df798e656dd6e4
Zepalesque <60141811+Zepalesque@users.noreply.github.com>
2024-03-03 14:24:43 -0500

    fix: aaaaaaaaaaaajdfgwjakrygnesiufeorewtgg

--- src/main/java/net/zepalesque/redux/data/loot/ReduxBlockLootData.java
+++ src/main/java/net/zepalesque/redux/data/loot/ReduxBlockLootData.java
@@ -88,6 +88,7 @@ public class ReduxBlockLootData extends AetherBlockLootSubProvider {
         this.dropPottedContents(ReduxBlocks.POTTED_BLIGHTWILLOW_SAPLING.get());
         this.dropPottedContents(ReduxBlocks.POTTED_CRYSTAL_SAPLING.get());
         this.dropPottedContents(ReduxBlocks.POTTED_CRYSTAL_FRUIT_SAPLING.get());
+        this.dropPottedContents(ReduxBlocks.POTTED_PURPLE_CRYSTAL_FRUIT_SAPLING.get());
 
 
         this.dropSelf(ReduxBlocks.GILDED_VINES.get());
