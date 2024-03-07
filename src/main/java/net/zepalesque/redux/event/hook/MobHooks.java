8924a690697745ad22a9a4b5032d676317359030
Zepalesque <60141811+Zepalesque@users.noreply.github.com>
2024-03-06 19:02:09 -0500

    refactor: Temporarily disabled unfinished swet stuff

--- src/main/java/net/zepalesque/redux/event/hook/MobHooks.java
+++ src/main/java/net/zepalesque/redux/event/hook/MobHooks.java
@@ -56,19 +56,23 @@ public class MobHooks {
             cockatrice.goalSelector.addGoal(2, new CockatriceRangedStrafeGoal(cockatrice, 1.0, 60, 10.0F));
         }
     }
-
+/*
     // TODO
     public static void modifySwetAI(Swet swet) {
-        final WrappedGoal[] toRemove = new WrappedGoal[1];
-        final WrappedGoal[] removeRanged = new WrappedGoal[1];
+        final WrappedGoal[] toRemove = new WrappedGoal[2];
         swet.targetSelector.getAvailableGoals().forEach((goal) -> {
-            if ((goal.getGoal().getClass().equals(HurtByTargetGoal.class) && goal.getPriority() == 1)) {
-                toRemove[0] = goal;}});
+            if ((goal.getGoal().getClass().equals(Swet.ConsumeGoal.class) && goal.getPriority() == 0)) {
+                toRemove[0] = goal;
+            }
+            if ((goal.getGoal().getClass().equals(Swet.HuntGoal.class) && goal.getPriority() == 1)) {
+                toRemove[1] = goal;
+            }
+        });
         if (toRemove[0] != null) {
             swet.targetSelector.getAvailableGoals().removeIf((wrappedGoal -> wrappedGoal == toRemove[0]));
         }
 
-    }
+    }*/
 
     public static void modifyEntityQuicksoil(final LivingEntity entity)
     {
