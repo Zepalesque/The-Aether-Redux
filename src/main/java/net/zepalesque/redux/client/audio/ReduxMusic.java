edce680cc4a5a07748bef49e755524f0b7fda102
Zepalesque <60141811+Zepalesque@users.noreply.github.com>
2024-03-07 19:27:24 -0500

    feat: Looping music

--- src/main/java/net/zepalesque/redux/client/audio/ReduxMusic.java
+++ src/main/java/net/zepalesque/redux/client/audio/ReduxMusic.java
@@ -5,5 +5,5 @@ import net.minecraft.sounds.Music;
 
 public class ReduxMusic {
     public static final Music DEFAULT_AETHER_MUSIC = new Music(AetherSoundEvents.MUSIC_AETHER.getHolder().orElseThrow(), 12000, 24000, true);
-    public static final Music REDUX_MENU = new Music(ReduxSoundEvents.REDUX_MENU.getHolder().orElseThrow(), 20, 600, true);
+    public static final Music REDUX_MENU = new Music(ReduxSoundEvents.REDUX_MENU.getHolder().orElseThrow(), 0, 0, true);
 }
