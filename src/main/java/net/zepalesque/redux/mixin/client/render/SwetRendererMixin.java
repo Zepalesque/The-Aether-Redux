96d7f42f26ff6dea1c76d0955a383be20c08de51
Zepalesque <60141811+Zepalesque@users.noreply.github.com>
2024-03-09 14:21:52 -0500

    fix: Festive swet compat

--- src/main/java/net/zepalesque/redux/mixin/client/render/SwetRendererMixin.java
+++ src/main/java/net/zepalesque/redux/mixin/client/render/SwetRendererMixin.java
@@ -3,6 +3,7 @@ package net.zepalesque.redux.mixin.client.render;
 import com.aetherteam.aether.client.renderer.entity.SwetRenderer;
 import com.aetherteam.aether.entity.monster.Swet;
 import com.mojang.blaze3d.vertex.PoseStack;
+import net.builderdog.ancient_aether.client.renderer.entity.renderer.FestiveSwetRenderer;
 import net.minecraft.client.model.SlimeModel;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.util.Mth;
@@ -14,7 +15,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(SwetRenderer.class)
+@Mixin(value = { SwetRenderer.class, FestiveSwetRenderer.class })
 public class SwetRendererMixin extends MobRendererMixin<Swet, SlimeModel<Swet>> {
 
     @Inject(at = @At("HEAD"), method = "scale(Lnet/minecraft/world/entity/LivingEntity;Lcom/mojang/blaze3d/vertex/PoseStack;F)V", cancellable = true)
