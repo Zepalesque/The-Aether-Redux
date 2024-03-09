6452590f975dc40a842be136f581899993e4cd7a
Zepalesque <60141811+Zepalesque@users.noreply.github.com>
2024-03-09 16:26:34 -0500

    refactor: alter swet speed

--- src/main/java/net/zepalesque/redux/mixin/common/entity/SwetMixin.java
+++ src/main/java/net/zepalesque/redux/mixin/common/entity/SwetMixin.java
@@ -131,7 +131,7 @@ public abstract class SwetMixin extends SlimeMixin {
             this.reapplyPosition();
             this.refreshDimensions();
             this.getAttribute(Attributes.MAX_HEALTH).setBaseValue((double) (i * i));
-            this.getAttribute(Attributes.MOVEMENT_SPEED).setBaseValue((double) (0.2F + 0.1F * (float) i));
+            this.getAttribute(Attributes.MOVEMENT_SPEED).setBaseValue((double) (0.3F + 0.05F * (float) i));
         this.getAttribute(Attributes.ATTACK_DAMAGE).setBaseValue((double)i * 0.5D);
             if (resetHealth) {
                 this.setHealth(this.getMaxHealth());
