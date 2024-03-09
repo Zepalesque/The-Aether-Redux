e2823ee284856d35972f352c44adc3c684c696b8
Zepalesque <60141811+Zepalesque@users.noreply.github.com>
2024-03-09 17:12:50 -0500

    feat: New pro tip message

--- src/main/java/net/zepalesque/redux/data/ReduxLanguageData.java
+++ src/main/java/net/zepalesque/redux/data/ReduxLanguageData.java
@@ -703,6 +703,7 @@ public class ReduxLanguageData extends AetherLanguageProvider {
         addProTip("snailshell_shield", "Feeding a Mykapod a Shimmerstool will make it shed the outer layer of its shell. These shell fragments can be used to craft a useful accessory that will decrease knockback.");
         addProTip("divinite", "Divinite can be enchanted into Glowstone, giving a relatively easy way to leave the Aether safely if you get stuck.");
         addProTip("redux_configs", "The Aether: Redux has a large amount of configs, including many that affect the base mod, so make sure to check out its config file!");
+        addProTip("swet_escape", "You can escape a Swet's gelatinous grasp by sneaking. It will still attack you though, so be quick!");
 
         for (WoodHandler woodHandler : Redux.WoodHandlers.WOOD_HANDLERS) {
             woodHandler.generateLanguageData(this);
