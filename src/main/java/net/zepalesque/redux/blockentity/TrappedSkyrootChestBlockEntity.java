4aaea88b5f517caed1f462bea08888f50fa28800
Zepalesque <60141811+Zepalesque@users.noreply.github.com>
2024-03-17 21:45:16 -0400

    fix: Trapped Chest stuff

--- src/main/java/net/zepalesque/redux/blockentity/TrappedSkyrootChestBlockEntity.java
+++ src/main/java/net/zepalesque/redux/blockentity/TrappedSkyrootChestBlockEntity.java
@@ -1,6 +1,8 @@
 package net.zepalesque.redux.blockentity;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.entity.ChestBlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
@@ -17,5 +19,15 @@ public class TrappedSkyrootChestBlockEntity extends ChestBlockEntity {
 
     public TrappedSkyrootChestBlockEntity() {
         this(ReduxBlockEntityTypes.TRAPPED_SKYROOT_CHEST.get(), BlockPos.ZERO, ReduxBlocks.TRAPPED_SKYROOT_CHEST.get().defaultBlockState());
+    }
+
+    protected void signalOpenCount(Level level, BlockPos pos, BlockState state, int eventId, int eventParam) {
+        super.signalOpenCount(level, pos, state, eventId, eventParam);
+        if (eventId != eventParam) {
+            Block block = state.getBlock();
+            level.updateNeighborsAt(pos, block);
+            level.updateNeighborsAt(pos.below(), block);
+        }
+
     }
 }
