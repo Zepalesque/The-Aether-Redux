b8f8f793422273ab57536b4c3cc847424fdd5b17
Zepalesque <60141811+Zepalesque@users.noreply.github.com>
2024-03-17 21:34:22 -0400

    fix: Okay apparently it needs them all

--- src/main/java/net/zepalesque/redux/mixin/common/entity/MimicMixin.java
+++ src/main/java/net/zepalesque/redux/mixin/common/entity/MimicMixin.java
@@ -26,7 +26,7 @@ public class MimicMixin {
     }
 
     @WrapWithCondition(method = "hurt", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/level/ServerLevel;sendParticles(Lnet/minecraft/core/particles/ParticleOptions;DDDIDDDD)I"))
-    private boolean hurt(ServerLevel level, ParticleOptions particle) {
+    private boolean hurt(ServerLevel level, ParticleOptions particle, double posX, double posY, double posZ, int particleCount, double xOffset, double yOffset, double zOffset, double speed) {
         return ((Mimic) (Object) this).getType() != ReduxEntityTypes.SKYROOT_MIMIC.get();
     }
 
