af1cd4eda4eebc5104d559ded26a349d3cc286b5
Zepalesque <60141811+Zepalesque@users.noreply.github.com>
2024-03-18 22:01:45 -0400

    refactor: improve version logic in case an aether 1.3.1 comes out

--- src/main/java/net/zepalesque/redux/mixin/common/registry/AdvancementSoundsMixin.java
+++ src/main/java/net/zepalesque/redux/mixin/common/registry/AdvancementSoundsMixin.java
@@ -5,11 +5,8 @@ import com.aetherteam.aether.api.AetherAdvancementSoundOverrides;
 import com.aetherteam.aether.api.registers.AdvancementSoundOverride;
 import net.minecraft.advancements.Advancement;
 import net.minecraft.sounds.SoundEvent;
-import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.fml.ModList;
 import net.minecraftforge.registries.RegistryObject;
-import net.zepalesque.redux.Redux;
 import net.zepalesque.redux.misc.ReduxTags;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
@@ -24,11 +21,11 @@ import java.lang.module.ModuleDescriptor;
 public class AdvancementSoundsMixin {
 
         @Unique
-        private static final boolean before140 = ModuleDescriptor.Version.parse(ModList.get().getModFileById(Aether.MODID).versionString()).compareTo(ModuleDescriptor.Version.parse("1.20.1-1.4.0-neoforge")) < 0;
+        private static final boolean notAfter1_3_0 = ModuleDescriptor.Version.parse(ModList.get().getModFileById(Aether.MODID).versionString()).compareTo(ModuleDescriptor.Version.parse("1.20.1-1.3.0-neoforge")) <= 0;
 
-        @Inject(method = "retrieveOverride", at = @At(value = "HEAD"), cancellable = true)
+        @Inject(method = "retrieveOverride", at = @At(value = "HEAD"), cancellable = true, remap = false)
         protected static void redux$retrieve(Advancement advancement, CallbackInfoReturnable<SoundEvent> cir) {
-                if (before140) {
+                if (notAfter1_3_0) {
                         @Nullable RegistryObject<AdvancementSoundOverride> reg = null;
                         for (RegistryObject<AdvancementSoundOverride> ov : AetherAdvancementSoundOverrides.ADVANCEMENT_SOUND_OVERRIDES.getEntries()) {
                                 if (ov.get().matches(advancement)) {
