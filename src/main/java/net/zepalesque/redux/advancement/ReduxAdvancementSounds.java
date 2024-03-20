baa27b216b3607bc981bdb3cfbdd11ab6a434874
Zepalesque <60141811+Zepalesque@users.noreply.github.com>
2024-03-19 21:43:56 -0400

    fix: forgot this

--- src/main/java/net/zepalesque/redux/advancement/ReduxAdvancementSounds.java
+++ src/main/java/net/zepalesque/redux/advancement/ReduxAdvancementSounds.java
@@ -2,10 +2,7 @@ package net.zepalesque.redux.advancement;
 
 import com.aetherteam.aether.api.AetherAdvancementSoundOverrides;
 import com.aetherteam.aether.api.registers.AdvancementSoundOverride;
-import com.aetherteam.aether.client.AetherSoundEvents;
-import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraftforge.registries.DeferredRegister;
-import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 import net.zepalesque.redux.Redux;
 import net.zepalesque.redux.client.audio.ReduxSoundEvents;
@@ -14,7 +11,7 @@ public class ReduxAdvancementSounds {
 
     public static final DeferredRegister<AdvancementSoundOverride> SOUNDS = DeferredRegister.create(AetherAdvancementSoundOverrides.ADVANCEMENT_SOUND_OVERRIDE_REGISTRY_KEY, Redux.MODID);
 
-    public static final RegistryObject<AdvancementSoundOverride> GOLD = SOUNDS.register("gold_dungeon", () -> new AdvancementSoundOverride(
+    public static final RegistryObject<AdvancementSoundOverride> GOLD = SOUNDS.register("gold_dungeon", () -> new AdvancementSoundOverride(10,
             advancement -> advancement.getId().getPath().equals("gold_dungeon"),
             ReduxSoundEvents.ADVANCEMENT_GOLD));
 
