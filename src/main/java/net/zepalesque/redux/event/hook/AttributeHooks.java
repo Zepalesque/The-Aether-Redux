341e0042542147dabfc614f85eef59f432aef647
Zepalesque <60141811+Zepalesque@users.noreply.github.com>
2024-03-25 20:59:46 -0400

    fix: why was this a mixin (didn't even work)

--- src/main/java/net/zepalesque/redux/event/hook/AttributeHooks.java
+++ src/main/java/net/zepalesque/redux/event/hook/AttributeHooks.java
@@ -2,6 +2,7 @@ package net.zepalesque.redux.event.hook;
 
 import com.aetherteam.aether.entity.AetherEntityTypes;
 import net.minecraft.world.entity.ai.attributes.Attributes;
+import net.minecraftforge.common.ForgeMod;
 import net.minecraftforge.event.entity.EntityAttributeModificationEvent;
 
 public class AttributeHooks {
@@ -9,6 +10,8 @@ public class AttributeHooks {
     public static void addAttributes(EntityAttributeModificationEvent event)
     {
         event.add(AetherEntityTypes.COCKATRICE.get(), Attributes.ATTACK_DAMAGE, 3.0D);
+        event.add(AetherEntityTypes.FLYING_COW.get(), ForgeMod.ENTITY_GRAVITY.get(), 0.0D);
+        event.add(AetherEntityTypes.PHYG.get(), ForgeMod.ENTITY_GRAVITY.get(), 0.0D);
 //        event.add(AetherEntityTypes.GOLDEN_SWET.get(), Attributes.ATTACK_DAMAGE, 3.0D);
 
     }
