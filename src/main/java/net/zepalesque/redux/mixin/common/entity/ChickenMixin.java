23d3be6dddf01b80779b5c7242f42bcfcb444b24
Zepalesque <60141811+Zepalesque@users.noreply.github.com>
2024-03-29 19:47:28 -0400

    feat: 'Quails' lay rebux

--- src/main/java/net/zepalesque/redux/mixin/common/entity/ChickenMixin.java
+++ src/main/java/net/zepalesque/redux/mixin/common/entity/ChickenMixin.java
@@ -1,10 +1,19 @@
 package net.zepalesque.redux.mixin.common.entity;
 
+import com.aetherteam.aether.entity.monster.dungeon.Mimic;
 import com.aetherteam.aether.entity.monster.dungeon.Sentry;
+import com.llamalad7.mixinextras.injector.v2.WrapWithCondition;
 import net.minecraft.sounds.SoundEvent;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.animal.Chicken;
+import net.minecraft.world.entity.item.ItemEntity;
+import net.minecraft.world.level.ItemLike;
+import net.zepalesque.redux.capability.animation.mimic.MimicAnimation;
 import net.zepalesque.redux.client.audio.ReduxSoundEvents;
+import net.zepalesque.redux.config.ReduxConfig;
+import net.zepalesque.redux.entity.misc.Rebux;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
@@ -31,5 +40,21 @@ public abstract class ChickenMixin extends MobMixin {
         if (this.hasCustomName() && "Quail".equals(this.getName().getString())) {
             cir.setReturnValue(ReduxSoundEvents.QUAIL_DEATH.get());
         }
+    }
+
+
+    @WrapWithCondition(method = "aiStep", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/animal/Chicken;spawnAtLocation(Lnet/minecraft/world/level/ItemLike;)Lnet/minecraft/world/entity/item/ItemEntity;"))
+    private boolean openEvent(Chicken instance, ItemLike itemLike) {
+        return this.redux$replaceEgg();
+    }
+
+    @Unique
+    private boolean redux$replaceEgg() {
+        if (this.hasCustomName() && "Quail".equals(this.getName().getString())) {
+            Rebux rebux = new Rebux(this.level(), this.getX(), this.getY(), this.getZ(), 1);
+            this.level().addFreshEntity(rebux);
+            return false;
+        }
+        return true;
     }
 }
--- src/main/java/net/zepalesque/redux/mixin/common/entity/EntityMixin.java
+++ src/main/java/net/zepalesque/redux/mixin/common/entity/EntityMixin.java
@@ -3,6 +3,7 @@ package net.zepalesque.redux.mixin.common.entity;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.syncher.SynchedEntityData;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -11,6 +12,14 @@ import javax.annotation.Nullable;
 
 @Mixin(Entity.class)
 public abstract class EntityMixin {
+
+    @Shadow public abstract double getZ();
+
+    @Shadow public abstract double getY();
+
+    @Shadow public abstract double getX();
+
+    @Shadow public abstract Level level();
 
     @Shadow public abstract Component getName();
 
