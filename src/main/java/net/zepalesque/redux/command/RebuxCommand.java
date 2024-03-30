570188a58a4c26e30fbb0720d0d61bab5c2c1f49
Zepalesque <60141811+Zepalesque@users.noreply.github.com>
2024-03-29 23:23:54 -0400

    fix: rebux command fixes

--- src/main/java/net/zepalesque/redux/command/RebuxCommand.java
+++ src/main/java/net/zepalesque/redux/command/RebuxCommand.java
@@ -35,12 +35,12 @@ public class RebuxCommand {
                                                 IntegerArgumentType.integer(0))
                                         .executes((context) -> setRebux(context.getSource(), GameProfileArgument.getGameProfiles(context, "targets"), IntegerArgumentType.getInteger(context, "value"))))
                         )
-                        .then(Commands.literal("set")
+                        .then(Commands.literal("add")
                                 .then(Commands.argument("value",
                                                 IntegerArgumentType.integer(0))
-                                        .executes((context) -> setRebux(context.getSource(), GameProfileArgument.getGameProfiles(context, "targets"), IntegerArgumentType.getInteger(context, "value"))))
-                        ).then(Commands.literal("query")).executes((context) -> queryRebux(context.getSource(), GameProfileArgument.getGameProfiles(context, "targets")))
-
+                                        .executes((context) -> addRebux(context.getSource(), GameProfileArgument.getGameProfiles(context, "targets"), IntegerArgumentType.getInteger(context, "value"))))
+                        ).then(Commands.literal("query")
+                        .executes((context) -> queryRebux(context.getSource(), GameProfileArgument.getGameProfiles(context, "targets"))))
                 )
         );
     }
