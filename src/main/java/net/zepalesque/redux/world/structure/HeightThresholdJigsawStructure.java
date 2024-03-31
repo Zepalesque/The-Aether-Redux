155306f3e2871bf70b99a2dd0e981194da3fba5e
Zepalesque <60141811+Zepalesque@users.noreply.github.com>
2024-03-30 21:10:43 -0400

    feat: HOPEFULLY got rid of dungeons in the void

---
+++ src/main/java/net/zepalesque/redux/world/structure/HeightThresholdJigsawStructure.java
@@ -0,0 +1,109 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
+package net.zepalesque.redux.world.structure;
+
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.DataResult;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
+import java.util.Optional;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Holder;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.ExtraCodecs;
+import net.minecraft.world.level.ChunkPos;
+import net.minecraft.world.level.levelgen.Heightmap;
+import net.minecraft.world.level.levelgen.WorldGenerationContext;
+import net.minecraft.world.level.levelgen.Heightmap.Types;
+import net.minecraft.world.level.levelgen.heightproviders.HeightProvider;
+import net.minecraft.world.level.levelgen.structure.Structure;
+import net.minecraft.world.level.levelgen.structure.StructureType;
+import net.minecraft.world.level.levelgen.structure.TerrainAdjustment;
+import net.minecraft.world.level.levelgen.structure.pools.JigsawPlacement;
+import net.minecraft.world.level.levelgen.structure.pools.StructureTemplatePool;
+import org.jetbrains.annotations.NotNull;
+
+public final class HeightThresholdJigsawStructure extends Structure {
+    public static final int MAX_TOTAL_STRUCTURE_RANGE = 128;
+    public static final Codec<HeightThresholdJigsawStructure> CODEC = ExtraCodecs.validate(RecordCodecBuilder.mapCodec((builder) -> {
+        return builder.group(settingsCodec(builder), StructureTemplatePool.CODEC.fieldOf("start_pool").forGetter((structure) -> {
+            return structure.startPool;
+        }), ResourceLocation.CODEC.optionalFieldOf("start_jigsaw_name").forGetter((structure) -> {
+            return structure.startJigsawName;
+        }), Codec.intRange(0, 7).fieldOf("size").forGetter((structure) -> {
+            return structure.maxDepth;
+        }), HeightProvider.CODEC.fieldOf("start_height").forGetter((structure) -> {
+            return structure.startHeight;
+        }), Codec.BOOL.fieldOf("use_expansion_hack").forGetter((structure) -> {
+            return structure.useExpansionHack;
+        }), Types.CODEC.optionalFieldOf("project_start_to_heightmap").forGetter((structure) -> {
+            return structure.projectStartToHeightmap;
+        }), Codec.intRange(1, MAX_TOTAL_STRUCTURE_RANGE).fieldOf("max_distance_from_center").forGetter((structure) -> {
+            return structure.maxDistanceFromCenter;
+        }), Codec.INT.optionalFieldOf("min_height").forGetter((structure) -> {
+            return structure.heightMin;
+        }), Codec.INT.optionalFieldOf("max_height").forGetter((structure) -> {
+            return structure.heightMax;
+        })).apply(builder, HeightThresholdJigsawStructure::new);
+    }), HeightThresholdJigsawStructure::verifyRange).codec();
+    private final Holder<StructureTemplatePool> startPool;
+    private final Optional<ResourceLocation> startJigsawName;
+    private final int maxDepth;
+    private final HeightProvider startHeight;
+    private final Optional<Integer> heightMin;
+    private final Optional<Integer> heightMax;
+    private final boolean useExpansionHack;
+    private final Optional<Heightmap.Types> projectStartToHeightmap;
+    private final int maxDistanceFromCenter;
+
+    private static DataResult<HeightThresholdJigsawStructure> verifyRange(HeightThresholdJigsawStructure structure) {
+        byte b0;
+        TerrainAdjustment terrainAdjustment = structure.terrainAdaptation();
+        if (terrainAdjustment == TerrainAdjustment.NONE) {
+            b0 = 0;
+        } else if (terrainAdjustment == TerrainAdjustment.BURY || terrainAdjustment == TerrainAdjustment.BEARD_THIN || terrainAdjustment == TerrainAdjustment.BEARD_BOX) {
+            b0 = 12;
+        } else {
+            throw new IncompatibleClassChangeError();
+        }
+
+        return structure.maxDistanceFromCenter + b0 > MAX_TOTAL_STRUCTURE_RANGE ? DataResult.error(() -> "Structure size including terrain adaptation must not exceed " + MAX_TOTAL_STRUCTURE_RANGE) : DataResult.success(structure);
+    }
+
+    public HeightThresholdJigsawStructure(Structure.StructureSettings settings, Holder<StructureTemplatePool> startPool, Optional<ResourceLocation> startJigsawName, int maxDepth, HeightProvider startHeight, boolean useExpansionHack, Optional<Heightmap.Types> projectStartToHeightmap, int maxDistanceToCenter, Optional<Integer> minY, Optional<Integer> maxY) {
+        super(settings);
+        this.startPool = startPool;
+        this.startJigsawName = startJigsawName;
+        this.maxDepth = maxDepth;
+        this.startHeight = startHeight;
+        this.useExpansionHack = useExpansionHack;
+        this.projectStartToHeightmap = projectStartToHeightmap;
+        this.maxDistanceFromCenter = maxDistanceToCenter;
+        this.heightMin = minY;
+        this.heightMax = maxY;
+    }
+
+    public HeightThresholdJigsawStructure(Structure.StructureSettings settings, Holder<StructureTemplatePool> startPool, int maxDepth, HeightProvider startHeight, boolean useExpansionHack, Heightmap.Types projectStartToHeightmap, Optional<Integer> minY, Optional<Integer> maxY) {
+        this(settings, startPool, Optional.empty(), maxDepth, startHeight, useExpansionHack, Optional.of(projectStartToHeightmap), 80, minY, maxY);
+    }
+
+    public HeightThresholdJigsawStructure(Structure.StructureSettings settings, Holder<StructureTemplatePool> startPool, int maxDepth, HeightProvider startHeight, boolean useExpansionHack, Optional<Integer> minY, Optional<Integer> maxY) {
+        this(settings, startPool, Optional.empty(), maxDepth, startHeight, useExpansionHack, Optional.empty(), 80, minY, maxY);
+    }
+
+    public @NotNull Optional<Structure.GenerationStub> findGenerationPoint(Structure.GenerationContext context) {
+        ChunkPos chunkpos = context.chunkPos();
+        int i = this.startHeight.sample(context.random(), new WorldGenerationContext(context.chunkGenerator(), context.heightAccessor()));
+        if ((this.heightMin.isPresent() && i < this.heightMin.get()) || (this.heightMax.isPresent() && i > this.heightMax.get())) {
+            return Optional.empty();
+        }
+        BlockPos blockpos = new BlockPos(chunkpos.getMinBlockX(), i, chunkpos.getMinBlockZ());
+        return JigsawPlacement.addPieces(context, this.startPool, this.startJigsawName, this.maxDepth, blockpos, this.useExpansionHack, this.projectStartToHeightmap, this.maxDistanceFromCenter);
+    }
+
+    public StructureType<?> type() {
+        return ReduxStructureTypes.HEIGHT_THRESHOLD_JIGSAW.get();
+    }
+}
--- src/main/java/net/zepalesque/redux/world/structure/ReduxStructureTypes.java
+++ src/main/java/net/zepalesque/redux/world/structure/ReduxStructureTypes.java
@@ -2,6 +2,7 @@ package net.zepalesque.redux.world.structure;
 
 import net.minecraft.core.registries.Registries;
 import net.minecraft.world.level.levelgen.structure.StructureType;
+import net.minecraft.world.level.levelgen.structure.structures.JigsawStructure;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.RegistryObject;
 import net.zepalesque.redux.Redux;
@@ -9,4 +10,5 @@ import net.zepalesque.redux.Redux;
 public class ReduxStructureTypes {
     public static final DeferredRegister<StructureType<?>> STRUCTURE_TYPES = DeferredRegister.create(Registries.STRUCTURE_TYPE, Redux.MODID);
     public static final RegistryObject<StructureType<EmptyStructure>> EMPTY = STRUCTURE_TYPES.register("empty", () -> () -> EmptyStructure.CODEC);
+    public static final RegistryObject<StructureType<HeightThresholdJigsawStructure>> HEIGHT_THRESHOLD_JIGSAW = STRUCTURE_TYPES.register("height_threshold_jigsaw", () -> () -> HeightThresholdJigsawStructure.CODEC);
 }
