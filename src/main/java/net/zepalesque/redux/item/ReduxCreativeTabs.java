624720bd20d62963657ee3c87d8d8e9abfaab6ef
Zepalesque <60141811+Zepalesque@users.noreply.github.com>
2024-04-10 23:57:58 -0400

    fix: i forgor :skull:

--- src/main/java/net/zepalesque/redux/item/ReduxCreativeTabs.java
+++ src/main/java/net/zepalesque/redux/item/ReduxCreativeTabs.java
@@ -272,7 +272,8 @@ public class ReduxCreativeTabs {
         if (tab == AetherCreativeTabs.AETHER_ARMOR_AND_ACCESSORIES.get()) {
 
             putAfter(AetherItems.VALKYRIE_CAPE, ReduxItems.GRAND_VICTORY_MEDAL, event);
-            putAfter(ReduxItems.GRAND_VICTORY_MEDAL, ReduxItems.COCKATRICE_FEATHER, event);
+            putAfter(ReduxItems.GRAND_VICTORY_MEDAL, ReduxItems.VALKYRIE_RING, event);
+            putAfter(ReduxItems.VALKYRIE_RING, ReduxItems.COCKATRICE_FEATHER, event);
             putAfter(ReduxItems.COCKATRICE_FEATHER, ReduxItems.FEATHER_OF_WARDING, event);
 
             putAfter(AetherItems.ICE_PENDANT, ReduxItems.ENCHANTED_RING, event);
