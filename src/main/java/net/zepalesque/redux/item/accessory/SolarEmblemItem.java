f0c1a19846a95079b859f7617b808762c3c2c1e0
Zepalesque <60141811+Zepalesque@users.noreply.github.com>
2024-04-11 23:00:04 -0400

    feat; Key on solar emblem tooltip

--- src/main/java/net/zepalesque/redux/item/accessory/SolarEmblemItem.java
+++ src/main/java/net/zepalesque/redux/item/accessory/SolarEmblemItem.java
@@ -1,10 +1,26 @@
 package net.zepalesque.redux.item.accessory;
 
-public class SolarEmblemItem extends AbilityTooltipMiscItem {
-    public SolarEmblemItem(Properties properties, String... pAbilities) {
-        super(properties, pAbilities);
+import com.aetherteam.aether.item.accessories.AccessoryItem;
+import net.minecraft.ChatFormatting;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.ItemStack;
+import net.zepalesque.redux.client.keys.ReduxKeys;
+
+import java.util.List;
+
+public class SolarEmblemItem extends AccessoryItem {
+
+    public SolarEmblemItem(Properties properties) {
+        super(properties);
     }
 
+    @Override
+    public List<Component> getAttributesTooltip(List<Component> tagTooltips, ItemStack stack) {
+        tagTooltips.add(Component.empty());
+        tagTooltips.add(Component.translatable("curios.modifiers.aether_accessory").withStyle(ChatFormatting.GOLD));
+        tagTooltips.add(Component.translatable("gui.aether_redux.solar_emblem_fireball", ReduxKeys.SHOOT_FIREBALL.getKey().getName()).withStyle(ChatFormatting.BLUE));
+        return super.getAttributesTooltip(tagTooltips, stack);
+    }
 
 
 }
