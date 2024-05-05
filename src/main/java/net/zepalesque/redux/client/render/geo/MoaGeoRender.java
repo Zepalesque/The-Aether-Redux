ea90c3ca718c5fdcf9a1d6dfe5afb89dd3ab78cf
Zepalesque <60141811+Zepalesque@users.noreply.github.com>
2024-05-05 14:16:06 -0400

    feat: stuff

--- src/main/java/net/zepalesque/redux/client/render/geo/MoaGeoRender.java
+++ src/main/java/net/zepalesque/redux/client/render/geo/MoaGeoRender.java
@@ -2,20 +2,17 @@ package net.zepalesque.redux.client.render.geo;
 
 import com.aetherteam.aether.entity.passive.Moa;
 import com.mojang.blaze3d.vertex.PoseStack;
-import com.mojang.math.Axis;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
-import net.minecraft.client.renderer.entity.LivingEntityRenderer;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.util.Mth;
+import net.minecraft.world.phys.Vec3;
 import net.zepalesque.redux.Redux;
 import net.zepalesque.redux.entity.geo.GeoMoa;
-import net.zepalesque.redux.entity.passive.Mykapod;
-import org.joml.Vector3f;
+import org.jetbrains.annotations.Nullable;
 import software.bernie.geckolib.core.animation.AnimationState;
+import software.bernie.geckolib.core.molang.MolangParser;
 import software.bernie.geckolib.model.GeoModel;
-import software.bernie.geckolib.renderer.GeoEntityRenderer;
 import software.bernie.geckolib.renderer.GeoReplacedEntityRenderer;
 
 public class MoaGeoRender extends GeoReplacedEntityRenderer<Moa, GeoMoa> {
@@ -30,6 +27,7 @@ public class MoaGeoRender extends GeoReplacedEntityRenderer<Moa, GeoMoa> {
 
     @Override
     public void render(Moa entity, float entityYaw, float partialTick, PoseStack poseStack, MultiBufferSource bufferSource, int packedLight) {
+        ((MoaGeoModel)this.model).setup(this.currentEntity, partialTick);
         super.render(entity, entityYaw, partialTick, poseStack, bufferSource, packedLight);
     }
 
@@ -46,32 +44,44 @@ public class MoaGeoRender extends GeoReplacedEntityRenderer<Moa, GeoMoa> {
         private static final ResourceLocation GEO_LOCATION = Redux.locate("geo/mykapod.geo.json");
         private static final ResourceLocation ANIM_LOCATION = Redux.locate("animations/mykapod.animation.json");
 
+        private @Nullable Moa current = null;
+        private float partial;
+
         @Override
-        public ResourceLocation getModelResource(GeoMoa mykapod) {
+        public ResourceLocation getModelResource(GeoMoa moa) {
             return GEO_LOCATION;
         }
 
         @Override
-        public ResourceLocation getTextureResource(GeoMoa mykapod) {
+        public ResourceLocation getTextureResource(GeoMoa moa) {
             // TODO
             return null;
         }
 
+        public void setup(Moa moa, float partial) {
+            this.current = moa;
+            this.partial = partial;
+        }
+
         @Override
-        public ResourceLocation getAnimationResource(GeoMoa mykapod) {
+        public ResourceLocation getAnimationResource(GeoMoa moa) {
             return ANIM_LOCATION;
         }
 
         @Override
-        public void setCustomAnimations(GeoMoa animatable, long instanceId, AnimationState<GeoMoa> animationState) {
+        public void setCustomAnimations(GeoMoa moa, long instanceId, AnimationState<GeoMoa> animationState) {
             // TODO: Head rotation
-            super.setCustomAnimations(animatable, instanceId, animationState);
+            super.setCustomAnimations(moa, instanceId, animationState);
         }
 
         @Override
         public void applyMolangQueries(GeoMoa animatable, double animTime) {
-            animatable.
             super.applyMolangQueries(animatable, animTime);
+            Minecraft mc = Minecraft.getInstance();
+            MolangParser parser = MolangParser.INSTANCE;
+            Moa m = this.current;
+            parser.setMemoizedValue("query.limb_swing", () -> m.walkAnimation.position(partial));
+            parser.setMemoizedValue("query.limb_swing_amount", () -> m.walkAnimation.speed(partial));
         }
     }
 }
--- src/main/java/net/zepalesque/redux/entity/geo/GeoMoa.java
+++ src/main/java/net/zepalesque/redux/entity/geo/GeoMoa.java
@@ -1,33 +1,93 @@
 package net.zepalesque.redux.entity.geo;
 
 import com.aetherteam.aether.entity.AetherEntityTypes;
+import com.aetherteam.aether.entity.passive.Moa;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import software.bernie.geckolib.animatable.GeoReplacedEntity;
+import software.bernie.geckolib.constant.DataTickets;
 import software.bernie.geckolib.core.animatable.instance.AnimatableInstanceCache;
 import software.bernie.geckolib.core.animation.AnimatableManager;
+import software.bernie.geckolib.core.animation.Animation;
 import software.bernie.geckolib.core.animation.AnimationController;
 import software.bernie.geckolib.core.animation.AnimationState;
+import software.bernie.geckolib.core.animation.RawAnimation;
 import software.bernie.geckolib.core.object.PlayState;
 import software.bernie.geckolib.util.GeckoLibUtil;
 
 public class GeoMoa implements GeoReplacedEntity {
-    private final AnimatableInstanceCache cache;
+    private final AnimatableInstanceCache cache = GeckoLibUtil.createInstanceCache(this);
 
     @Override
     public EntityType<?> getReplacingEntityType() {
         return AetherEntityTypes.MOA.get();
-        this.cache = GeckoLibUtil.createInstanceCache(this);
-        this.cache.addDataPoint();
     }
 
     @Override
     public void registerControllers(AnimatableManager.ControllerRegistrar registrar) {
-        registrar.add(new AnimationController<>(this, "wings", 3, this::wingAnim));
+        registrar.add(new AnimationController<>(this, "wings", 5, this::wingAnim));
+        registrar.add(new AnimationController<>(this, "legs", 5, this::legAnim));
+        registrar.add(new AnimationController<>(this, "legs", 10, this::bodyAnim));
+        registrar.add(new AnimationController<>(this, "hurt", 10, this::hurtAnim));
     }
 
+    private static final String WINGS_FLY_START = "animation.moa.wings.fly_start";
+    private static final String WINGS_FLY = "animation.moa.wings.fly";
+    private static final String WINGS_GLIDE = "animation.moa.wings.glide";
+    private static final String WINGS_IDLE = "animation.moa.wings.idle";
+
+    private static final String BODY_WALK = "animation.moa.body.walk";
+    private static final String BODY_FLY = "animation.moa.body.fly";
+    private static final String BODY_GLIDE = "animation.moa.body.glide";
+    private static final String BODY_IDLE = "animation.moa.body.idle";
+
+    private static final String BODY_HURT = "animation.moa.body.hurt";
+
+    private static final String LEGS_WALK = "animation.moa.legs.walk";
+    private static final String LEGS_FLY = "animation.moa.legs.fly";
+
     private PlayState wingAnim(AnimationState<GeoMoa> state) {
-        this.cache.addDataPoint();
+        Moa moa = (Moa) state.getData(DataTickets.ENTITY);
+
+        if (!moa.isEntityOnGround()) {
+            state.getController().setAnimation(RawAnimation.begin().thenPlayXTimes(WINGS_FLY_START, 1)
+                    .then(state.isMoving() ? WINGS_GLIDE : WINGS_FLY, Animation.LoopType.LOOP));
+            return PlayState.CONTINUE;
+        } else {
+
+            state.getController().setAnimation(RawAnimation.begin().thenPlay(WINGS_IDLE));
+            return PlayState.CONTINUE;
+        }
     }
+    private PlayState legAnim(AnimationState<GeoMoa> state) {
+        Moa moa = (Moa) state.getData(DataTickets.ENTITY);
+
+        state.getController().setAnimation(RawAnimation.begin().thenLoop(moa.isEntityOnGround() ? LEGS_WALK : LEGS_FLY));
+        return PlayState.CONTINUE;
+    }
+
+    private PlayState bodyAnim(AnimationState<GeoMoa> state) {
+        Moa moa = (Moa) state.getData(DataTickets.ENTITY);
+
+        if (!moa.isEntityOnGround()) {
+            state.getController().setAnimation(RawAnimation.begin().thenLoop(state.isMoving() ? BODY_GLIDE : BODY_FLY));
+            return PlayState.CONTINUE;
+        } else {
+
+            state.getController().setAnimation(RawAnimation.begin().thenLoop(state.isMoving() ? BODY_WALK : BODY_IDLE));
+            return PlayState.CONTINUE;
+        }
+    }
+
+    private PlayState hurtAnim(AnimationState<GeoMoa> state) {
+        Moa moa = (Moa) state.getData(DataTickets.ENTITY);
+
+        if (moa.hurtTime == moa.hurtDuration) {
+            state.getController().setAnimation(RawAnimation.begin().thenPlayXTimes(BODY_HURT, 1));
+        }
+        return PlayState.CONTINUE;
+    }
+
 
     public AnimatableInstanceCache getAnimatableInstanceCache() {
         return this.cache;
