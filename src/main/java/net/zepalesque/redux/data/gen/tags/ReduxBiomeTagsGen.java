617397e9a3b18d4a5c3f47f51434c797e973ea52
Zepalesque <60141811+Zepalesque@users.noreply.github.com>
2024-06-01 18:29:05 -0400

    fix: forgor

--- src/main/java/net/zepalesque/redux/data/gen/tags/ReduxBiomeTagsGen.java
+++ src/main/java/net/zepalesque/redux/data/gen/tags/ReduxBiomeTagsGen.java
@@ -1,10 +1,12 @@
 package net.zepalesque.redux.data.gen.tags;
 
+import com.aetherteam.aether.AetherTags;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
 import net.minecraft.data.tags.BiomeTagsProvider;
 import net.neoforged.neoforge.common.data.ExistingFileHelper;
 import net.zepalesque.redux.Redux;
+import net.zepalesque.redux.data.ReduxTags;
 
 import javax.annotation.Nullable;
 import java.util.concurrent.CompletableFuture;
@@ -18,6 +20,9 @@ public class ReduxBiomeTagsGen extends BiomeTagsProvider {
     @SuppressWarnings("unchecked")
     @Override
     public void addTags(HolderLookup.Provider provider) {
-
+        
+        this.tag(ReduxTags.Biomes.HAS_CLOUDBED).addTag(AetherTags.Biomes.IS_AETHER);
+        this.tag(ReduxTags.Biomes.MODIFY_MUSIC).addTag(AetherTags.Biomes.IS_AETHER);
+        this.tag(ReduxTags.Biomes.MODIFY_SKY_COLOR).addTag(AetherTags.Biomes.IS_AETHER);
     }
 }
