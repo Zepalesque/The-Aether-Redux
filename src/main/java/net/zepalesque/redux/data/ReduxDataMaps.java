689afb773e079f3614793662da2ef571be94513f
Zepalesque <60141811+Zepalesque@users.noreply.github.com>
2024-06-02 09:31:35 -0400

    feat: temporary test biome tints

--- src/main/java/net/zepalesque/redux/data/ReduxDataMaps.java
+++ src/main/java/net/zepalesque/redux/data/ReduxDataMaps.java
@@ -1,18 +1,5 @@
 package net.zepalesque.redux.data;
 
-import com.mojang.serialization.Codec;
-import net.minecraft.core.registries.BuiltInRegistries;
-import net.minecraft.core.registries.Registries;
-import net.minecraft.tags.TagKey;
-import net.minecraft.world.level.block.Block;
-import net.neoforged.neoforge.registries.datamaps.DataMapType;
-import net.zepalesque.redux.Redux;
-
 public class ReduxDataMaps {
-
-//        public static final DataMapType<Block, Integer> SHORT_GRASS_COLOR_CHANGERS = DataMapType.builder(Redux.loc("tint/override/short_grass_overrides"),
-//                Registries.BLOCK, Codec.INT)
-//                .synced(Codec.INT, false)
-//                .build();
 
 }
--- src/main/java/net/zepalesque/redux/data/gen/ReduxDataMapGen.java
+++ src/main/java/net/zepalesque/redux/data/gen/ReduxDataMapGen.java
@@ -1,13 +1,16 @@
 package net.zepalesque.redux.data.gen;
 
 import com.aetherteam.aether.data.generators.AetherDataMapData;
+import com.aetherteam.aether.data.resources.registries.AetherBiomes;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
+import net.neoforged.neoforge.registries.datamaps.builtin.NeoForgeDataMaps;
 import net.zepalesque.redux.Redux;
 import net.zepalesque.redux.client.ReduxColors;
 import net.zepalesque.redux.data.ReduxDataMaps;
 import net.zepalesque.redux.data.ReduxTags;
 import net.zepalesque.redux.data.prov.ReduxDataMapProvider;
+import net.zepalesque.redux.world.biome.tint.ReduxBiomeTints;
 
 import java.util.concurrent.CompletableFuture;
 
@@ -21,5 +24,11 @@ public class ReduxDataMapGen extends ReduxDataMapProvider {
     protected void gather() {
         Redux.WOOD_SETS.forEach(set -> set.mapData(this));
         Redux.STONE_SETS.forEach(set -> set.mapData(this));
+
+        var biomeTints = this.builder(ReduxBiomeTints.AETHER_GRASS.get().getDataMap());
+        biomeTints.add(AetherBiomes.SKYROOT_FOREST, 0x000000, false);
+        biomeTints.add(AetherBiomes.SKYROOT_GROVE, 0xFF0000, false);
+        biomeTints.add(AetherBiomes.SKYROOT_MEADOW, 0x00FF00, false);
+        biomeTints.add(AetherBiomes.SKYROOT_WOODLAND, 0x0000FF, false);
     }
 }
