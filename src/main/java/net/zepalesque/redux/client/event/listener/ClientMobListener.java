c00442d40b761f6fe43adeb0f307edf4ecada76d
Zepalesque <60141811+Zepalesque@users.noreply.github.com>
2024-06-02 08:55:01 -0400

    fix: add annotation

--- src/main/java/net/zepalesque/redux/client/event/listener/ClientMobListener.java
+++ src/main/java/net/zepalesque/redux/client/event/listener/ClientMobListener.java
@@ -2,12 +2,16 @@ package net.zepalesque.redux.client.event.listener;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.world.entity.LivingEntity;
+import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.Mod;
 import net.neoforged.neoforge.event.entity.living.LivingEvent;
+import net.zepalesque.redux.Redux;
 import net.zepalesque.redux.client.event.hook.KeyHooks;
 import net.zepalesque.redux.config.ReduxConfig;
 import net.zepalesque.redux.event.hook.QuicksoilHooks;
 
+@Mod.EventBusSubscriber(modid = Redux.MODID, value = Dist.CLIENT)
 public class ClientMobListener {
 
     @SubscribeEvent
