7d25bc80d12be48e4b8666b55ec6195ca2deb253
Zepalesque <60141811+Zepalesque@users.noreply.github.com>
2024-06-01 21:40:53 -0400

    chore: datagen

--- src/main/java/net/zepalesque/redux/data/gen/tags/ReduxBlockTagsGen.java
+++ src/main/java/net/zepalesque/redux/data/gen/tags/ReduxBlockTagsGen.java
@@ -1,9 +1,11 @@
 package net.zepalesque.redux.data.gen.tags;
 
+import com.aetherteam.aether.block.AetherBlocks;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
 import net.neoforged.neoforge.common.data.ExistingFileHelper;
 import net.zepalesque.redux.Redux;
+import net.zepalesque.redux.data.ReduxTags;
 import net.zepalesque.redux.data.prov.tags.ReduxBlockTagsProvider;
 import org.jetbrains.annotations.Nullable;
 
@@ -19,6 +21,8 @@ public class ReduxBlockTagsGen extends ReduxBlockTagsProvider {
     protected void addTags(HolderLookup.Provider provider) {
         Redux.WOOD_SETS.forEach(set -> set.blockTagData(this));
         Redux.STONE_SETS.forEach(set -> set.blockTagData(this));
+
+        this.tag(ReduxTags.Blocks.QUICKSOIL_BEHAVIOR).add(AetherBlocks.QUICKSOIL.get());
 
     }
 }
--- src/main/java/net/zepalesque/redux/mixin/mixins/common/QuicksoilMixin.java
+++ src/main/java/net/zepalesque/redux/mixin/mixins/common/QuicksoilMixin.java
@@ -10,7 +10,6 @@ import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.pathfinder.PathComputationType;
 import net.zepalesque.redux.config.ReduxConfig;
 import net.zepalesque.redux.mixin.mixins.common.base.BlockBehaviorMixin;
-import net.zepalesque.redux.mixin.mixins.common.base.BlockMixin;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
