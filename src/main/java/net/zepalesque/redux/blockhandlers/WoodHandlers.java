ef2aca250e382d51154a8a1c7c5d5cf1d357db89
Zepalesque <60141811+Zepalesque@users.noreply.github.com>
2024-06-09 16:07:08 -0400

    fix: fixes

--- src/main/java/net/zepalesque/redux/blockhandlers/WoodHandlers.java
+++ src/main/java/net/zepalesque/redux/blockhandlers/WoodHandlers.java
@@ -1,17 +1,17 @@
 package net.zepalesque.redux.blockhandlers;
 
 import net.minecraft.world.level.block.SoundType;
-import net.minecraft.world.level.material.MapColor;
+import net.minecraft.world.level.material.MaterialColor;
 import net.zepalesque.redux.Redux;
 import net.zepalesque.redux.api.blockhandler.WoodHandler;
 
 public class WoodHandlers {
-    public static final WoodHandler FIELDSPROOT = register(WoodHandler.handler("fieldsproot", null, true, WoodHandler.cherrySoundBlockSet(), "trees", "log", "wood", SoundType.CHERRY_WOOD, SoundType.CHERRY_WOOD, false, MapColor.NETHER, MapColor.COLOR_ORANGE, false, true));
-    public static final WoodHandler BLIGHTWILLOW = register(WoodHandler.handler("blightwillow", null, true, WoodHandler.bambooSoundBlockSet(), "trees", "log", "wood", SoundType.BAMBOO_WOOD, SoundType.BAMBOO_WOOD, true, MapColor.TERRACOTTA_CYAN, MapColor.COLOR_GREEN, true, false));
-    public static final WoodHandler CLOUDCAP = register(WoodHandler.fungus("cloudcap", true, MapColor.WOOL, MapColor.TERRACOTTA_PURPLE, false));
-    public static final WoodHandler JELLYSHROOM = register(WoodHandler.noStrippingFungus("jellyshroom", false, MapColor.COLOR_GRAY, MapColor.COLOR_GRAY, false));
-    public static final WoodHandler CRYSTAL = register(WoodHandler.tree("crystal", false, MapColor.TERRACOTTA_CYAN, MapColor.COLOR_LIGHT_BLUE, false));
-    public static final WoodHandler GLACIA = register(WoodHandler.tree("glacia", false, MapColor.TERRACOTTA_BLACK, MapColor.TERRACOTTA_LIGHT_GRAY, true));
+    public static final WoodHandler FIELDSPROOT = register(WoodHandler.handler("fieldsproot", null, true, "trees", "log", "wood", SoundType.WOOD, SoundType.WOOD, false, MaterialColor.NETHER, MaterialColor.COLOR_ORANGE, false, true));
+    public static final WoodHandler BLIGHTWILLOW = register(WoodHandler.handler("blightwillow", null, true,"trees", "log", "wood", SoundType.WOOD, SoundType.WOOD, true, MaterialColor.TERRACOTTA_CYAN, MaterialColor.COLOR_GREEN, true, false));
+    public static final WoodHandler CLOUDCAP = register(WoodHandler.fungus("cloudcap", true, MaterialColor.WOOL, MaterialColor.TERRACOTTA_PURPLE, false));
+    public static final WoodHandler JELLYSHROOM = register(WoodHandler.noStrippingFungus("jellyshroom", false, MaterialColor.COLOR_GRAY, MaterialColor.COLOR_GRAY, false));
+    public static final WoodHandler CRYSTAL = register(WoodHandler.tree("crystal", false, MaterialColor.TERRACOTTA_CYAN, MaterialColor.COLOR_LIGHT_BLUE, false));
+    public static final WoodHandler GLACIA = register(WoodHandler.tree("glacia", false, MaterialColor.TERRACOTTA_BLACK, MaterialColor.TERRACOTTA_LIGHT_GRAY, true));
 
     private static <T extends WoodHandler> T register(T handler) {
         Redux.WOOD_HANDLERS.add(handler);
