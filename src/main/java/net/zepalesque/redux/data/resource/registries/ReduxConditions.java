a11b679cc4e70080ad8bf853cb137b900e1e8da4
Zepalesque <60141811+Zepalesque@users.noreply.github.com>
2024-06-15 13:13:03 -0400

    fix: fix

--- src/main/java/net/zepalesque/redux/data/resource/registries/ReduxConditions.java
+++ src/main/java/net/zepalesque/redux/data/resource/registries/ReduxConditions.java
@@ -28,7 +28,7 @@ public class ReduxConditions {
         context.register(ANCIENT, new ModLoadedCondition("ancient_aether"));
         context.register(SKY_COLORS, new ConfigCondition(ReduxConfig.SERVER.serializerID(), ReduxConfig.SERVER.redux_sky_colors));
         context.register(CLOUDBED, new ConfigCondition(ReduxConfig.SERVER.serializerID(), ReduxConfig.SERVER.cloudbed));
-        context.register(RAW_ORES, new ConfigCondition(ReduxConfig.SERVER.serializerID(), ReduxConfig.SERVER.cloudbed));
+        context.register(RAW_ORES, new ConfigCondition(ReduxConfig.SERVER.serializerID(), ReduxConfig.SERVER.raw_ores));
     }
 
     private static ResourceKey<Condition<?>> createKey(String name) {
