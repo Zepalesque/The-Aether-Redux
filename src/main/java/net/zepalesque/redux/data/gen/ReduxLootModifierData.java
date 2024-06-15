5211cb28ed34bd4d2b51900abac5a361903cfecd
Zepalesque <60141811+Zepalesque@users.noreply.github.com>
2024-06-15 15:04:09 -0400

    fix: stuff

--- src/main/java/net/zepalesque/redux/data/gen/ReduxLootModifierData.java
+++ src/main/java/net/zepalesque/redux/data/gen/ReduxLootModifierData.java
@@ -1,5 +1,6 @@
 package net.zepalesque.redux.data.gen;
 
+import net.minecraft.core.Holder;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
 import net.minecraft.world.item.ItemStack;
@@ -12,11 +13,13 @@ import net.minecraft.world.level.storage.loot.predicates.LootItemCondition;
 import net.minecraft.world.level.storage.loot.providers.number.ConstantValue;
 import net.zepalesque.redux.Redux;
 import net.zepalesque.redux.block.ReduxBlocks;
+import net.zepalesque.redux.config.ReduxConfig;
 import net.zepalesque.redux.data.prov.ReduxLootModifierProvider;
 import net.zepalesque.redux.data.resource.registries.ReduxConditions;
 import net.zepalesque.redux.item.ReduxItems;
 import net.zepalesque.redux.loot.modifer.RawOreModifier;
 import net.zepalesque.zenith.Zenith;
+import net.zepalesque.zenith.api.condition.ConfigCondition;
 import net.zepalesque.zenith.loot.condition.ConditionLootModule;
 
 import java.util.concurrent.CompletableFuture;
@@ -34,7 +37,8 @@ public class ReduxLootModifierData extends ReduxLootModifierProvider {
                         ApplyBonusCount.addOreBonusCount(Enchantments.BLOCK_FORTUNE).build()
                 },
                 new LootItemCondition[] {
-                        new ConditionLootModule(provider.lookupOrThrow(Zenith.Keys.CONDITION).getOrThrow(ReduxConditions.RAW_ORES)),
+                        // It appears that LootModifierManager is being a bit buggy, so just use a direct inline holder instead of the key
+                        new ConditionLootModule(Holder.direct(new ConfigCondition("redux_server", ReduxConfig.SERVER.raw_ores))),
                         HAS_SILK_TOUCH.invert().build(),
                         LootItemBlockStatePropertyCondition.hasBlockStateProperties(ReduxBlocks.VERIDIUM_ORE.get()).build()}));
 
