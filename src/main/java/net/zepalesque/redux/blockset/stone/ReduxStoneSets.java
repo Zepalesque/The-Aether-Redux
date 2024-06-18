05ae0b7b8e8e521b2796f1a134d12d332072cdbd
Zepalesque <60141811+Zepalesque@users.noreply.github.com>
2024-06-17 22:07:18 -0400

    fix: fixes

--- src/main/java/net/zepalesque/redux/blockset/stone/ReduxStoneSets.java
+++ src/main/java/net/zepalesque/redux/blockset/stone/ReduxStoneSets.java
@@ -18,27 +18,6 @@ import net.zepalesque.zenith.api.blockset.util.CraftingMatrix;
 
 public class ReduxStoneSets {
 
-    public static final BaseStoneSet SENTRITE = register(new BaseStoneSet("sentrite", MapColor.DEEPSLATE, SoundType.NETHER_ORE, 1.0F, 6.0F, "natural/",
-            "A dark, metallic rock found throughout the Aether. This crude metal is used in a number of parts of Sentry technology, even their walls of Carved Stone are made of a mixture of this and Holystone.")).craftsInto(AetherBlocks.CARVED_STONE,
-            new CraftingMatrix(4, builder ->
-                    builder
-                            .define('H', AetherBlocks.HOLYSTONE.get())
-                            .pattern("#H")
-                            .pattern("H#")))
-            .craftsIntoSet(() -> ReduxStoneSets.SENTRITE_BRICKS, CraftingMatrices.SQUARE_2X2)
-            .stonecutIntoSet(() -> ReduxStoneSets.SENTRITE_BRICKS)
-            .creativeTab(AetherCreativeTabs.AETHER_BUILDING_BLOCKS, () -> ReduxStoneSets.BLIGHTMOSS_HOLYSTONE.wall().get(), true)
-            .creativeTab(AetherCreativeTabs.AETHER_NATURAL_BLOCKS, () -> ReduxStoneSets.BLIGHTMOSS_HOLYSTONE.block().get(), false)
-            .withTag(BlockTags.MINEABLE_WITH_PICKAXE, true);
-
-    public static final BaseStoneSet SENTRITE_BRICKS = register(new BrickStoneSet("sentrite_brick", MapColor.DEEPSLATE, SoundType.NETHER_BRICKS, 1.0F, 6.0F, "construction/",
-            "Bricks made of Sentrite. These can be used as a nice building block!"))
-            .creativeTab(AetherCreativeTabs.AETHER_BUILDING_BLOCKS, () -> ReduxStoneSets.SENTRITE.wall().get(), true)
-            .withTag(BlockTags.MINEABLE_WITH_PICKAXE, true)
-            .withTag(AetherTags.Blocks.DUNGEON_BLOCKS, false)
-            .withTag(AetherTags.Blocks.SENTRY_BLOCKS, false)
-            .withItemTag(AetherTags.Items.SENTRY_BLOCKS, false);
-
     public static final BaseStoneSet GILDED_HOLYSTONE = register(new BaseStoneSet("gilded_holystone", MapColor.SAND, SoundType.STONE, 0.5F, 0.5F, "natural/",
             "The enchanted form of Mossy Holystone. This rock covered in golden moss glitters in the sunlight."))
             .creativeTab(AetherCreativeTabs.AETHER_BUILDING_BLOCKS, () -> AetherBlocks.MOSSY_HOLYSTONE_WALL, true)
@@ -50,6 +29,28 @@ public class ReduxStoneSets {
             .creativeTab(AetherCreativeTabs.AETHER_BUILDING_BLOCKS, ReduxStoneSets.GILDED_HOLYSTONE::wall, true)
             .creativeTab(AetherCreativeTabs.AETHER_NATURAL_BLOCKS, ReduxStoneSets.GILDED_HOLYSTONE::block, false)
             .withTag(BlockTags.MINEABLE_WITH_PICKAXE, true);
+
+    public static final BaseStoneSet SENTRITE = register(new BaseStoneSet("sentrite", MapColor.DEEPSLATE, SoundType.NETHER_ORE, 1.0F, 6.0F, "natural/",
+            "A dark, metallic rock found throughout the Aether. This crude metal is used in a number of parts of Sentry technology, even their walls of Carved Stone are made of a mixture of this and Holystone.")).craftsInto(AetherBlocks.CARVED_STONE,
+                    new CraftingMatrix(4, builder ->
+                            builder
+                                    .define('H', AetherBlocks.HOLYSTONE.get())
+                                    .pattern("#H")
+                                    .pattern("H#")))
+            .craftsIntoSet(() -> ReduxStoneSets.SENTRITE_BRICKS, CraftingMatrices.SQUARE_2X2)
+            .stonecutIntoSet(() -> ReduxStoneSets.SENTRITE_BRICKS)
+            .creativeTab(AetherCreativeTabs.AETHER_BUILDING_BLOCKS, ReduxStoneSets.BLIGHTMOSS_HOLYSTONE::wall, true)
+            .creativeTab(AetherCreativeTabs.AETHER_NATURAL_BLOCKS, ReduxStoneSets.BLIGHTMOSS_HOLYSTONE::block, false)
+            .withTag(BlockTags.MINEABLE_WITH_PICKAXE, true);
+
+    public static final BaseStoneSet SENTRITE_BRICKS = register(new BrickStoneSet("sentrite_brick", MapColor.DEEPSLATE, SoundType.NETHER_BRICKS, 1.0F, 6.0F, "construction/",
+            "Bricks made of Sentrite. These can be used as a nice building block!"))
+            .creativeTab(AetherCreativeTabs.AETHER_BUILDING_BLOCKS, () -> ReduxStoneSets.SENTRITE.wall().get(), true)
+            .withTag(BlockTags.MINEABLE_WITH_PICKAXE, true)
+            .withTag(AetherTags.Blocks.DUNGEON_BLOCKS, false)
+            .withTag(AetherTags.Blocks.SENTRY_BLOCKS, false)
+            .withItemTag(AetherTags.Items.SENTRY_BLOCKS, false);
+
 
 
     public static <T extends AbstractStoneSet> T register(T set) {
