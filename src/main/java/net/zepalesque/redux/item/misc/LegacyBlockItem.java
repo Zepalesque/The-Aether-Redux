2cf93c2f4c5104b27815634db8fa1d35d9f241f6
Zepalesque <60141811+Zepalesque@users.noreply.github.com>
2024-07-11 10:27:38 -0400

    improv: improvements

--- src/main/java/net/zepalesque/redux/item/misc/LegacyBlockItem.java
+++ src/main/java/net/zepalesque/redux/item/misc/LegacyBlockItem.java
@@ -1,5 +1,6 @@
 package net.zepalesque.redux.item.misc;
 
+import com.google.common.base.Supplier;
 import net.minecraft.ChatFormatting;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.MutableComponent;
@@ -8,12 +9,10 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
-import net.zepalesque.redux.item.tools.VeridiumItem;
 import net.zepalesque.redux.item.util.TooltipUtils;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
-import com.google.common.base.Supplier;
 
 public class LegacyBlockItem extends BlockItem {
     private final Supplier<? extends Block> replacement;
@@ -25,7 +24,7 @@ public class LegacyBlockItem extends BlockItem {
 
     @Override
     public void appendHoverText(ItemStack stack, @Nullable Level level, List<Component> tooltips, TooltipFlag advanced) {
-        MutableComponent infusion = Component.translatable("tooltip.aether_redux.legacy_item").withStyle(ChatFormatting.GRAY);
+        MutableComponent infusion = Component.translatable("tooltip.aether_redux.legacy_item").withStyle(ChatFormatting.YELLOW);
 
         tooltips.add(infusion);
         Component info = TooltipUtils.TOOLTIP_SHIFT_FOR_INFO.apply(Component.translatable("tooltip.aether_redux.item_replacement", this.replacement.get().getName()).withStyle(ChatFormatting.GRAY));
