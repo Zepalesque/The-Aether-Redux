3773b695cfad1aa4389eeed525f536d747225697
Zepalesque <60141811+Zepalesque@users.noreply.github.com>
2025-02-06 15:27:04 -0500

    revert: Undo name change

--- src/main/java/net/zepalesque/redux/api/WeightedParticleEntry.java
+++ src/main/java/net/zepalesque/redux/api/WeightedParticleEntry.java
@@ -8,14 +8,12 @@ import net.minecraft.core.particles.ParticleOptions;
 import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.util.RandomSource;
 
-import java.util.Optional;
+public record WeightedParticleEntry(ParticleOptions particle, Either<Integer, Float> probability) {
 
-public record WeightedParticleEntry(ParticleOptions particle, Optional<Either<Integer, Float>> probability) {
-
-    private static final MapCodec<Optional<Either<Integer, Float>>> PROBABILITY_CODEC = Codec.mapEither(
-            Codec.INT.optionalFieldOf("rarity"),
-            Codec.FLOAT.optionalFieldOf("chance")
-    ).xmap(WeightedParticleEntry::mapToOptional, WeightedParticleEntry::mapFromOptional);
+    private static final MapCodec<Either<Integer, Float>> PROBABILITY_CODEC = Codec.mapEither(
+            Codec.INT.fieldOf("rarity"),
+            Codec.FLOAT.fieldOf("chance")
+    );
 
     public static WeightedParticleEntry of(ParticleOptions particle, int rarity) {
         return new WeightedParticleEntry(particle, Either.left(rarity));
@@ -29,10 +27,6 @@ public record WeightedParticleEntry(ParticleOptions particle, Optional<Either<In
         return new WeightedParticleEntry(particle, Either.left(15));
     }
 
-    WeightedParticleEntry(ParticleOptions particle, Either<Integer, Float> either) {
-        this(particle, Optional.of(either));
-    }
-
     public static final Codec<WeightedParticleEntry> CODEC = RecordCodecBuilder.create(builder -> builder.group(
             ParticleTypes.CODEC.fieldOf("particle").forGetter(WeightedParticleEntry::particle),
             PROBABILITY_CODEC.forGetter(WeightedParticleEntry::probability)
@@ -42,21 +36,6 @@ public record WeightedParticleEntry(ParticleOptions particle, Optional<Either<In
 
 
     public boolean success(RandomSource random) {
-        return this.probability().map(e -> e.map(i -> random.nextInt(i) == 0, f -> random.nextFloat() < f)).orElse(random.nextInt(15) == 0);
-    }
-
-
-
-
-    private static Either<Optional<Integer>, Optional<Float>> mapFromOptional(Optional<Either<Integer, Float>> optional) {
-        return optional.map(e -> e.mapBoth(Optional::ofNullable, Optional::ofNullable)).orElse(Either.left(Optional.empty()));
-    }
-
-    private static Optional<Either<Integer, Float>> mapToOptional(Either<Optional<Integer>, Optional<Float>> either) {
-        var wrapped = Optional.of(either);
-
-        boolean valuePresent = either.map(Optional::isPresent, Optional::isPresent);
-
-        return valuePresent ? wrapped.map(e -> e.mapBoth(Optional::get, Optional::get)) : Optional.empty();
+        return this.probability().map(i -> random.nextInt(i) == 0, f -> random.nextFloat() < f);
     }
 }
