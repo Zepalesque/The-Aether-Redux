dc94b23aa51ebb687e173881bff2217043e8aa17
Zepalesque <60141811+Zepalesque@users.noreply.github.com>
2025-03-19 23:49:20 -0400

    improv: Improve gilded non-enchanted grass color

--- src/main/java/net/zepalesque/redux/client/ReduxColors.java
+++ src/main/java/net/zepalesque/redux/client/ReduxColors.java
@@ -21,7 +21,7 @@ import java.util.function.Predicate;
 public class ReduxColors {
 
     public static class Tints {
-        public static final int GILDED_GRASS_COLOR = 0xFFF3FFDD;
+        public static final int GILDED_GRASS_COLOR = 0xFFFFCC99;
         public static final int BLIGHT_GRASS_COLOR = 0xFFBEAEE5;
 
         public static final int BLEAKMOSS_GRASS_COLOR = 0xFFB79EC1;
