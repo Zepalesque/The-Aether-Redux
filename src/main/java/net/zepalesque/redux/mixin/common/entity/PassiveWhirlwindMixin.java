2aba9c8750b4ae0cf770ab25780157a931f6c5b5
Zepalesque <60141811+Zepalesque@users.noreply.github.com>
2025-05-23 14:21:23 -0400

    fix: mixin issue

--- src/main/java/net/zepalesque/redux/mixin/common/entity/PassiveWhirlwindMixin.java
+++ src/main/java/net/zepalesque/redux/mixin/common/entity/PassiveWhirlwindMixin.java
@@ -10,7 +10,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(PassiveWhirlwind.class)
 public abstract class PassiveWhirlwindMixin extends AbstractWhirlwindMixin {
 
-    @Inject(method = "spawnParticles", at = @At(value = "HEAD"), cancellable = true)
+    @Inject(method = "spawnParticles", at = @At(value = "HEAD"), cancellable = true, remap = false)
     protected void redux$spawnParticles(CallbackInfo ci) {
         if (ReduxConfig.CLIENT.improved_whirlwinds.get()) ci.cancel();
     }
