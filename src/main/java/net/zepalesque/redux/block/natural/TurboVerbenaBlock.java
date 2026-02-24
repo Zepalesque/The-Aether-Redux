cf1490daf32b01d45bc3eb41d42571184a9e7e9f
junideergirl <ultrasquid.epic@gmail.com>
2026-02-24 16:22:24 -0500

    Turbo Verbena Texture Updates

--- src/main/java/net/zepalesque/redux/block/natural/TurboVerbenaBlock.java
+++ src/main/java/net/zepalesque/redux/block/natural/TurboVerbenaBlock.java
@@ -5,14 +5,30 @@ import com.aetherteam.aether.block.AetherBlocks;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Holder;
 import net.minecraft.world.effect.MobEffect;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.LevelReader;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.FlowerBlock;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.VoxelShape;
 
 public class TurboVerbenaBlock extends FlowerBlock {
+	private static final VoxelShape SHAPE = Block.box(1.0, 0.0, 1.0, 15.0, 9.0, 15.0);
+
 	public TurboVerbenaBlock(Holder<MobEffect> effect, float seconds, BlockBehaviour.Properties properties) {
 		super(effect, seconds, properties);
+	}
+
+	protected VoxelShape getShape(
+		BlockState state,
+		BlockGetter level,
+		BlockPos pos,
+		CollisionContext context
+	) {
+		var offset = state.getOffset(level, pos);
+		return SHAPE.move(offset.x, offset.y, offset.z);
 	}
 
 	@Override
