24a4d135f27de08fe01cda76db600b6f37792784
Zepalesque <60141811+Zepalesque@users.noreply.github.com>
2026-03-01 21:52:09 -0500

    feat: package info expansion task :3

---
+++ src/main/templates/package-info-template.java
@@ -0,0 +1,8 @@
+@ParametersAreNonnullByDefault
+@MethodsReturnNonnullByDefault
+@FieldsAreNonnullByDefault
+package ${directory};
+
+import net.minecraft.FieldsAreNonnullByDefault;
+import net.minecraft.MethodsReturnNonnullByDefault;
+import javax.annotation.ParametersAreNonnullByDefault;
