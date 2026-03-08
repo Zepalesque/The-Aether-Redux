d3e7a83bacb84e711be8779b2168258a9e3b2e5e
Zepalesque <60141811+Zepalesque@users.noreply.github.com>
2026-03-08 00:08:35 -0500

    chore: datagen

--- src/generated/java/net/zepalesque/redux/item/tools/package-info.java
+++ src/generated/java/net/zepalesque/redux/item/tools/package-info.java
@@ -3,6 +3,6 @@
 @FieldsAreNonnullByDefault
 package net.zepalesque.redux.item.tools;
 
+import javax.annotation.ParametersAreNonnullByDefault;
 import net.minecraft.FieldsAreNonnullByDefault;
 import net.minecraft.MethodsReturnNonnullByDefault;
-import javax.annotation.ParametersAreNonnullByDefault;
--- src/generated/java/net/zepalesque/redux/mixin/mixins/common/accessor/package-info.java
+++ src/generated/java/net/zepalesque/redux/mixin/mixins/common/accessor/package-info.java
@@ -3,6 +3,6 @@
 @FieldsAreNonnullByDefault
 package net.zepalesque.redux.mixin.mixins.common.accessor;
 
+import javax.annotation.ParametersAreNonnullByDefault;
 import net.minecraft.FieldsAreNonnullByDefault;
 import net.minecraft.MethodsReturnNonnullByDefault;
-import javax.annotation.ParametersAreNonnullByDefault;
