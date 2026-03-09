648de030961092c8be272ae4dd8b66a1e08904a2
Zepalesque <60141811+Zepalesque@users.noreply.github.com>
2026-03-09 19:45:49 -0400

    fix: ok wacky merge conflicts lmao

--- src/main/java/net/zepalesque/redux/world/tree/foliage/MoonfirFoliagePlacer.java
+++ src/main/java/net/zepalesque/redux/world/tree/foliage/MoonfirFoliagePlacer.java
@@ -4,47 +4,61 @@ import com.mojang.serialization.MapCodec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.minecraft.util.Mth;
 import net.minecraft.util.RandomSource;
+import net.minecraft.util.valueproviders.FloatProvider;
 import net.minecraft.util.valueproviders.IntProvider;
 import net.minecraft.world.level.LevelSimulatedReader;
 import net.minecraft.world.level.levelgen.feature.configurations.TreeConfiguration;
 import net.minecraft.world.level.levelgen.feature.foliageplacers.FoliagePlacer;
 import net.minecraft.world.level.levelgen.feature.foliageplacers.FoliagePlacerType;
 
-<<<<<<<< HEAD:src/main/java/net/zepalesque/redux/world/tree/foliage/LegacyGlaciaFoliagePlacer.java
-public class LegacyGlaciaFoliagePlacer extends FoliagePlacer {
-	public static final MapCodec<LegacyGlaciaFoliagePlacer> CODEC = RecordCodecBuilder.mapCodec(
-		builder -> foliagePlacerParts(builder)
-			.and(IntProvider.codec(0, 24).fieldOf("trunk_height").forGetter(f -> f.trunkHeight))
-			.apply(builder, LegacyGlaciaFoliagePlacer::new)
-========
 public class MoonfirFoliagePlacer extends FoliagePlacer {
 	public static final MapCodec<MoonfirFoliagePlacer> CODEC = RecordCodecBuilder.mapCodec(
 		builder -> foliagePlacerParts(builder)
-			.and(IntProvider.codec(0, 24).fieldOf("trunk_height").forGetter(f -> f.trunkHeight))
+			.and(IntProvider.codec(0, 24).fieldOf("trunk_height").forGetter(instance -> instance.trunkHeight))
+			.and(IntProvider.codec(1, 10).fieldOf("arms").forGetter(instance -> instance.arms))
+			.and(FloatProvider.CODEC.fieldOf("rotations").forGetter(instance -> instance.rotations))
+			.and(FloatProvider.CODEC.fieldOf("start_angle").forGetter(instance -> instance.initialAngle))
+			.and(FloatProvider.codec(0, 1).fieldOf("pointiness").forGetter(instance -> instance.initialAngle))
 			.apply(builder, MoonfirFoliagePlacer::new)
->>>>>>>> 7bb8417d62d74df351324043c27a02840995a254:src/main/java/net/zepalesque/redux/world/tree/foliage/MoonfirFoliagePlacer.java
 	);
 
 	private final IntProvider trunkHeight;
+	private final IntProvider arms;
+	private final FloatProvider rotations;
+	private final FloatProvider initialAngle;
+	private final FloatProvider pointiness;
 
-<<<<<<<< HEAD:src/main/java/net/zepalesque/redux/world/tree/foliage/LegacyGlaciaFoliagePlacer.java
-	public LegacyGlaciaFoliagePlacer(IntProvider radius, IntProvider offset, IntProvider trunkHeight) {
-========
-	public MoonfirFoliagePlacer(IntProvider radius, IntProvider offset, IntProvider trunkHeight) {
->>>>>>>> 7bb8417d62d74df351324043c27a02840995a254:src/main/java/net/zepalesque/redux/world/tree/foliage/MoonfirFoliagePlacer.java
+	public MoonfirFoliagePlacer(
+		IntProvider radius,
+		IntProvider offset,
+		IntProvider trunkHeight,
+		IntProvider arms,
+		FloatProvider rotations,
+		FloatProvider initialAngle,
+		FloatProvider pointiness
+	) {
 		super(radius, offset);
 		this.trunkHeight = trunkHeight;
+		this.arms = arms;
+		this.rotations = rotations;
+		this.initialAngle = initialAngle;
+		this.pointiness = pointiness;
 	}
 
 	protected FoliagePlacerType<?> type() {
-		return ReduxFoliagePlacers.MOONFIR.get();
+		return ReduxFoliagePlacers.GLACIA.get();
 	}
 
 	@Override
 	public int foliageHeight(RandomSource rand, int i, TreeConfiguration cfg) {
 		return this.trunkHeight.sample(rand) + 1;
 	}
-
+	
+	@Override
+	protected boolean shouldSkipLocation(RandomSource random, int localX, int localY, int localZ, int range, boolean large) {
+		return false;
+	}
+	
 	@Override
 	protected void createFoliage(
 		LevelSimulatedReader reader,
@@ -52,34 +66,52 @@ public class MoonfirFoliagePlacer extends FoliagePlacer {
 		RandomSource rand,
 		TreeConfiguration cfg,
 		int maxHeight,
-		FoliagePlacer.FoliageAttachment attachment,
+		FoliageAttachment attachment,
 		int height,
 		int radius,
 		int offset
 	) {
+		var arms = this.arms.sample(rand);
+		var rots = this.rotations.sample(rand);
+		var start = this.initialAngle.sample(rand);
+		var pointiness = this.pointiness.sample(rand);
+		
 		var origin = attachment.pos().above(offset + 1);
 
-		for (int i = 0; i < height; i++) {
-			this.placeLeavesRow(reader, setter, rand, cfg, origin, radius, -i, false);
-		}
+		for (var y = 0; y < height; y++)
+			for (var x = -radius; x <= radius; x++)
+				for (var z = -radius; z <= radius; z++)
+					if (testByHeight(x, y, z, start, rots, arms, height, radius, pointiness)) {
+						var pos = origin.offset(x, y, z);
+						setter.set(pos, cfg.foliageProvider.getState(rand, pos));
+					}
 	}
-
-	@Override
-	protected boolean shouldSkipLocation(
-		RandomSource rand,
+	
+	// Ensure parameters are consistent for each block
+	boolean testByHeight(
 		int x,
 		int y,
 		int z,
-		int radius,
-		boolean large
+		float startAngle,
+		float totalRotations,
+		int spiralArms,
+		int totalHeight,
+		float maxRad,
+		float pointiness
 	) {
-		final var x2 = (float)(x & 1) / 2f;
-		final var z2 = (float)(z & 1) / 2f;
-
-		final var delta = Math.log(-y) / (y < 4 ? 1 : 2);
-		final var maxDist = Mth.lerp(delta, 0, 1) + (float)rand.nextInt(-4, 4) / 10f;
-		final var dist = Mth.sqrt(Mth.square(x - x2) + Mth.square(z - z2));
+		var rSqr = x*x + z*z;
+		var theta = (float) Mth.atan2(y, x);
 		
-		return dist > maxDist;
+		var perc = y / totalHeight;
+		
+		var currRad = maxRad * perc;
+		
+		var sinInput = spiralArms * (theta + startAngle + totalRotations * Mth.TWO_PI * perc);
+		
+		var unscaledBound = currRad * Mth.sin(sinInput) + 10 * currRad / pointiness;
+		var bound = unscaledBound / (1 + 10 / pointiness);
+		
+		return rSqr <= bound*bound;
 	}
+
 }
