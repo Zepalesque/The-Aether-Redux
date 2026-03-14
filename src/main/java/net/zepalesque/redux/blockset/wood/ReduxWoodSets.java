fa63ea0ae91f6242179704d21d3e504be6bcec26
junideergirl <ultrasquid.epic@gmail.com>
2026-03-13 22:50:06 -0400

    Gildleaf Initial Work

--- src/main/java/net/zepalesque/redux/blockset/wood/ReduxWoodSets.java
+++ src/main/java/net/zepalesque/redux/blockset/wood/ReduxWoodSets.java
@@ -8,6 +8,7 @@ import net.zepalesque.redux.blockset.wood.type.RegularBookshelfSet;
 import net.zepalesque.zenith.api.blockset.type.AbstractWoodSet;
 
 public class ReduxWoodSets {
+	public static final LogWallWoodSet GILDLEAF = register(new RegularBookshelfSet("gildleaf", MapColor.TERRACOTTA_ORANGE, MapColor.TERRACOTTA_RED, SoundType.CHERRY_WOOD));
 	public static final LogWallWoodSet CRYSTAL = register(new RegularBookshelfSet("crystal", MapColor.COLOR_LIGHT_BLUE, MapColor.TERRACOTTA_CYAN, SoundType.WOOD));
 	public static final LogWallWoodSet MOONFIR = register(new RegularBookshelfSet("moonfir", MapColor.TERRACOTTA_BLACK, MapColor.TERRACOTTA_LIGHT_GRAY, SoundType.WOOD));
 	public static final LogWallWoodSet BLIGHTWILLOW = register(new RegularBookshelfSet("blightwillow", MapColor.COLOR_GREEN, MapColor.COLOR_GRAY, SoundType.CHERRY_WOOD));
