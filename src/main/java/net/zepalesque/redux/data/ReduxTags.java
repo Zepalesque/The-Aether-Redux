fdd460494baf3ccd9b14f536e5900784595b1582
Zepalesque <60141811+Zepalesque@users.noreply.github.com>
2026-03-15 22:27:44 -0400

    fix: 𝔱𝔞𝔤 𝔦𝔰𝔰𝔲𝔢
    skill issue but tag

--- src/main/java/net/zepalesque/redux/data/ReduxTags.java
+++ src/main/java/net/zepalesque/redux/data/ReduxTags.java
@@ -80,7 +80,7 @@ public class ReduxTags {
 		public static final TagKey<Biome> HAS_CAVES = tag("has_caves");
 		public static final TagKey<Biome> HAS_CONDITIONAL_SNOW = tag("has_conditional_snow");
 		
-		public static final TagKey<Biome> IS_FROSTED = tag("has_caves");
+		public static final TagKey<Biome> IS_FROSTED = tag("is_frosted");
 		
 		public static final TagKey<Biome> BLOOMTAIL_BONEMEAL = tag("bloomtail_bonemeal");
 		
