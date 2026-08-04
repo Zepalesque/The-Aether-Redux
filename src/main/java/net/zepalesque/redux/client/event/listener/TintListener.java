ea95f03689fcc13acf3343b225aefcff9afc3e18
Zepalesque <60141811+Zepalesque@users.noreply.github.com>
2026-08-04 13:55:51 -0400

    revert: undo said temp changes for now

--- src/main/java/net/zepalesque/redux/client/event/listener/TintListener.java
+++ src/main/java/net/zepalesque/redux/client/event/listener/TintListener.java
@@ -1,32 +1,20 @@
 package net.zepalesque.redux.client.event.listener;
 
-import net.minecraft.core.BlockPos;
-import net.minecraft.world.level.levelgen.XoroshiroRandomSource;
-import net.minecraft.world.level.levelgen.synth.PerlinNoise;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.client.event.RegisterColorHandlersEvent;
 import net.zepalesque.redux.block.ReduxBlocks;
 
-import java.util.stream.IntStream;
-
 @EventBusSubscriber(Dist.CLIENT)
 public class TintListener {
-	
-	public static final PerlinNoise PRISMA_NOISE = PerlinNoise.create(new XoroshiroRandomSource(2743), IntStream.of(0));
-	
 	@SubscribeEvent
 	public static void registerTintHandlers(RegisterColorHandlersEvent.Block event) {
 		event.register(
 			(state, level, pos, tintIndex) -> {
-				final var posScale = 50d;
-				final var sampleScale = 35d;
 				// TODO: null check level and pos which by the signature of BlockColor#getColor may be null
-				var samplePos = pos == null ? BlockPos.ZERO : pos;
-				var noise = PRISMA_NOISE.getValue(samplePos.getX() / posScale, samplePos.getY() / posScale, samplePos.getZ() / posScale) * sampleScale;
 				// Distance from 0, 0 using pythagorean theorem
-//				var dist = Math.sqrt(Math.pow(pos.getX(), 2) + Math.pow(pos.getZ(), 2));
+				var dist = Math.sqrt(Math.pow(pos.getX(), 2) + Math.pow(pos.getZ(), 2));
 
 				// Creating the color channels using the sine of the dist.
 				// The steps are:
@@ -34,10 +22,10 @@ public class TintListener {
 				// - Multiply the result by 128, so they go from -128:128 (range of a signed byte)
 				// - Convert to a byte
 				// - Add 128, to account for signedness
-				var r = (byte)(Math.sin(noise) * 128) + 128;
-				var g = (byte)(-Math.sin(noise) * 128) + 128;
-				var b1 = (byte)(Math.cos(noise) * 128) + 128;
-				var b2 = (byte)(-Math.cos(noise) * 128) + 128;
+				var r = (byte)(Math.sin(dist) * 128) + 128;
+				var g = (byte)(-Math.sin(dist) * 128) + 128;
+				var b1 = (byte)(Math.cos(dist) * 128) + 128;
+				var b2 = (byte)(-Math.cos(dist) * 128) + 128;
 				var b = Math.max(b1, b2);
 
 				// Clamping the values to create a pastel look
