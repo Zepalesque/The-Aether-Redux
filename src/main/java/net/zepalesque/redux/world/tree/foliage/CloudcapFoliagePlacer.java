d063034aa8f0c9ff29263209722e21ff63fb83a0
junideergirl <ultrasquid.epic@gmail.com>
2026-08-15 20:43:59 -0400

    Mushrom Thigny :3333

--- src/main/java/net/zepalesque/redux/world/tree/foliage/CloudcapFoliagePlacer.java
+++ src/main/java/net/zepalesque/redux/world/tree/foliage/CloudcapFoliagePlacer.java
@@ -1,8 +1,13 @@
 package net.zepalesque.redux.world.tree.foliage;
 
+import com.ibm.icu.impl.Pair;
 import com.mojang.serialization.MapCodec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.function.IntUnaryOperator;
+
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.BlockPos.MutableBlockPos;
 import net.minecraft.core.Direction;
@@ -10,11 +15,13 @@ import net.minecraft.util.RandomSource;
 import net.minecraft.util.valueproviders.ConstantInt;
 import net.minecraft.util.valueproviders.IntProvider;
 import net.minecraft.world.level.LevelSimulatedReader;
+import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.levelgen.feature.configurations.TreeConfiguration;
 import net.minecraft.world.level.levelgen.feature.foliageplacers.FoliagePlacer;
 import net.minecraft.world.level.levelgen.feature.foliageplacers.FoliagePlacerType;
 import net.minecraft.world.level.levelgen.feature.stateproviders.BlockStateProvider;
+import net.zepalesque.redux.block.natural.CloudCapBlock;
 
 public class CloudcapFoliagePlacer extends FoliagePlacer {
 	public static final MapCodec<CloudcapFoliagePlacer> CODEC = RecordCodecBuilder.mapCodec(
@@ -51,18 +58,30 @@ public class CloudcapFoliagePlacer extends FoliagePlacer {
 		int radius,
 		int offset
 	) {
-		var pos = attachment.pos().mutable();
-		tryPlaceLeaf(level, setter, rand, cfg, pos);
-
+		final var capSetter = new CapSetter(level, setter, rand, cfg);
 		final var dirsToCut = rand.nextBoolean();
-
 		final IntUnaryOperator cutAmount = i -> (i % 2 == 0) ^ dirsToCut ? 0 : 1;
+
+		var pos = attachment.pos();
+
+		capSetter.set(pos, List.of(
+			Direction.DOWN,
+			Direction.NORTH,
+			Direction.SOUTH,
+			Direction.EAST,
+			Direction.WEST
+		));
 
 		for (var dir : Direction.Plane.HORIZONTAL) {
 			var offsetPos = pos.mutable().move(dir);
 
-			tryPlaceLeaf(level, setter, rand, cfg, offsetPos);
-			tryPlaceLeaf(level, setter, rand, cfg, offsetPos.move(Direction.DOWN));
+			capSetter.set(offsetPos, List.of(dir.getOpposite(), Direction.DOWN));
+			capSetter.set(offsetPos.move(Direction.DOWN), List.of(
+				Direction.UP,
+				Direction.DOWN,
+				dir,
+				dir.getOpposite()
+			));
 
 			var nettingHeight = rand.nextInt(height - 1, height + 1);
 			for (var j = 0; j < nettingHeight; j++) {
@@ -74,26 +93,20 @@ public class CloudcapFoliagePlacer extends FoliagePlacer {
 		for (var dir : Direction.Plane.HORIZONTAL) {
 			var h = height - cutAmount.applyAsInt(dir.ordinal());
 			var offsetPos = pos.mutable().move(dir, 2);
-
-			for (var j = 0; j < h; j++) {
-				tryPlaceLeaf(level, setter, rand, cfg, offsetPos.move(Direction.DOWN));
-			}
+			capSetter.setColumn(offsetPos, h, List.of(dir.getOpposite()));
 		}
 
-		MutableBlockPos[] positions = {
-			pos.below().north().west().mutable(),
-			pos.below().north().east().mutable(),
-			pos.below().south().east().mutable(),
-			pos.below().south().west().mutable(),
-		};
-
-		for (var i = 0; i < positions.length; ++i) {
-			var h = height - cutAmount.applyAsInt(i);
-			var otherPos = positions[i];
-
-			for (var j = 0; j < h - 1; j++) {
-				tryPlaceLeaf(level, setter, rand, cfg, otherPos.move(Direction.DOWN));
-			}
+		var dirs = List.of(
+			Pair.of(Direction.NORTH, Direction.EAST),
+			Pair.of(Direction.NORTH, Direction.WEST),
+			Pair.of(Direction.SOUTH, Direction.EAST),
+			Pair.of(Direction.SOUTH, Direction.WEST)
+		);
+		for (var i = 0; i < dirs.size(); ++i) {
+			var p = dirs.get(i);
+			var h = height - cutAmount.applyAsInt(i) - 1;
+			var offsetPos = pos.mutable().move(p.first).move(p.second).move(Direction.DOWN);
+			capSetter.setColumn(offsetPos, h, List.of(p.first.getOpposite(), p.second.getOpposite()));
 		}
 	}
 
@@ -132,5 +145,58 @@ public class CloudcapFoliagePlacer extends FoliagePlacer {
 	@Override
 	protected FoliagePlacerType<?> type() {
 		return ReduxFoliagePlacers.CLOUDCAP_FOLIAGE.get();
+	}
+
+	private static record CapSetter(
+		LevelSimulatedReader level,
+		FoliageSetter setter,
+		RandomSource rand,
+		TreeConfiguration cfg
+	) {
+		void set(BlockPos pos, List<Direction> dirs) {
+			// TODO: See if this can be optimized
+			tryPlaceLeaf(
+				level,
+				new FoliageSetter() {
+					@Override
+					public boolean isSet(BlockPos pos) {
+						return setter.isSet(pos);
+					}
+
+					@Override
+					public void set(BlockPos pos, BlockState state) {
+						if (state.getBlock() instanceof CloudCapBlock cap) state = cap.transform(
+							state
+								.setValue(BlockStateProperties.NORTH, !dirs.contains(Direction.NORTH))
+								.setValue(BlockStateProperties.SOUTH, !dirs.contains(Direction.SOUTH))
+								.setValue(BlockStateProperties.EAST, !dirs.contains(Direction.EAST))
+								.setValue(BlockStateProperties.WEST, !dirs.contains(Direction.WEST))
+								.setValue(BlockStateProperties.DOWN, !dirs.contains(Direction.DOWN))
+								.setValue(BlockStateProperties.UP, !dirs.contains(Direction.UP)),
+							pos
+						);
+						setter.set(pos, state);
+					}
+				},
+				rand,
+				cfg,
+				pos
+			);
+		}
+
+		void setColumn(MutableBlockPos pos, int height, List<Direction> dirs) {
+			var list = new ArrayList<>(dirs);
+
+			list.add(Direction.DOWN);
+			this.set(pos.move(Direction.DOWN), list);
+
+			list.add(Direction.UP);
+			for (var j = 1; j < height - 1; j++) {
+				this.set(pos.move(Direction.DOWN), list);
+			}
+
+			list.remove(Direction.DOWN);
+			this.set(pos.move(Direction.DOWN), list);
+		}
 	}
 }
