251cf12c327d22b528884cc33e55a94cec3b7b9f
Zepalesque <60141811+Zepalesque@users.noreply.github.com>
2026-08-22 13:24:27 -0400

    improv: use little private final class for reference-like stuff

--- src/main/java/net/zepalesque/redux/event/hook/MobHooks.java
+++ src/main/java/net/zepalesque/redux/event/hook/MobHooks.java
@@ -11,10 +11,13 @@ import net.zepalesque.redux.entity.ai.goal.CockatriceMeleeAttackGoal;
 import net.zepalesque.redux.entity.ai.goal.CockatriceRangedStrafeGoal;
 import net.zepalesque.redux.entity.ai.target.HurtByOtherTypeTargetGoal;
 
+import java.util.Objects;
+import java.util.Optional;
+
 public class MobHooks {
 
 	public static void modifyCockatriceAI(Cockatrice cockatrice) {
-		if (ReduxConfig.COMMON.cockatrice_burn_in_daylight.get()) {
+		if (ReduxConfig.SERVER.cockatrice_burn_in_daylight.get()) {
 			cockatrice.goalSelector.addGoal(2, new RestrictSunGoal(cockatrice));
 			cockatrice.goalSelector.addGoal(3, new FleeSunGoal(cockatrice, 1.0D));
 		}
@@ -22,20 +25,18 @@ public class MobHooks {
 			1,
 			new CockatriceMeleeAttackGoal(cockatrice, 1.5, false)
 		);
-		final WrappedGoal[] toRemove = new WrappedGoal[1];
-		final WrappedGoal[] removeRanged = new WrappedGoal[1];
-		cockatrice.targetSelector.getAvailableGoals().forEach((goal) -> {
+		final var ref = new GoalsRef();
+		
+		cockatrice.targetSelector.getAvailableGoals().forEach(goal -> {
 			if (
 				goal.getGoal().getClass().equals(HurtByTargetGoal.class) &&
 				goal.getPriority() == 1
-			) {
-				toRemove[0] = goal;
-			}
+			) ref.target = goal;
 		});
-		if (toRemove[0] != null) {
+		if (ref.target != null) {
 			cockatrice.targetSelector
 				.getAvailableGoals()
-				.removeIf((wrappedGoal) -> wrappedGoal == toRemove[0]);
+				.removeIf(wrappedGoal -> wrappedGoal == ref.target);
 			cockatrice.targetSelector.addGoal(
 				1,
 				new HurtByOtherTypeTargetGoal(cockatrice)
@@ -46,22 +47,47 @@ public class MobHooks {
 			new CockatriceMeleeAttackGoal(cockatrice, 1.5, false)
 		);
 
-		cockatrice.goalSelector.getAvailableGoals().forEach((goal) -> {
+		cockatrice.goalSelector.getAvailableGoals().forEach(goal -> {
 			if (
 				goal.getGoal().getClass().equals(RangedAttackGoal.class) &&
 				goal.getPriority() == 2
 			) {
-				removeRanged[0] = goal;
+				ref.goal = goal;
 			}
 		});
-		if (toRemove[0] != null) {
+		if (ref.goal != null) {
 			cockatrice.goalSelector
 				.getAvailableGoals()
-				.removeIf((wrappedGoal) -> wrappedGoal == removeRanged[0]);
+				.removeIf(wrappedGoal -> wrappedGoal == ref.goal);
 			cockatrice.goalSelector.addGoal(
 				2,
 				new CockatriceRangedStrafeGoal(cockatrice, 1.0, 60, 10.0F)
 			);
+		}
+	}
+	
+	private static final class GoalsRef {
+		WrappedGoal target = null;
+		WrappedGoal goal = null;
+		
+		@Override
+		public boolean equals(Object o) {
+			if (o == null || this.getClass() != o.getClass()) return false;
+			var goalsRef = (GoalsRef) o;
+			return Objects.equals(this.target, goalsRef.target) && Objects.equals(this.goal, goalsRef.goal);
+		}
+		
+		@Override
+		public int hashCode() {
+			return Objects.hash(this.target, this.goal);
+		}
+		
+		@Override
+		public String toString() {
+			return "GoalsRef[" +
+				"target=" + this.target +
+				", goal=" + this.goal +
+				']';
 		}
 	}
 }
